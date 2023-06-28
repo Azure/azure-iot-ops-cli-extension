6171c51c40ea36913a02b079b23c9e9f1ea710c1
Paymaun Heidari <digimaun@users.noreply.github.com>
2023-06-28 12:05:29 -0700

    WIP increment vers

--- azext_edge/constants.py
+++ azext_edge/constants.py
@@ -7,7 +7,7 @@
 
 import os
 
-VERSION = "0.0.1a2"
+VERSION = "0.0.1a3"
 EXTENSION_NAME = "azure-edge"
 EXTENSION_ROOT = os.path.dirname(os.path.abspath(__file__))
 USER_AGENT = "EdgePlatformCliExtension/{}".format(VERSION)
