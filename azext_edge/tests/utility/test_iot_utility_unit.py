3657ee7db3339f45630dd96d22b9b3e099121119
Paymaun <digimaun@users.noreply.github.com>
2023-07-11 14:49:31 -0700

    Update to NDA based license. (#5)

--- azext_edge/tests/utility/test_iot_utility_unit.py
+++ azext_edge/tests/utility/test_iot_utility_unit.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 # --------------------------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See License.txt in the project root for license information.
+# PRIVATE DISTRIBUTION FOR NDA CUSTOMERS ONLY
 # --------------------------------------------------------------------------------------------
 
 import os
@@ -44,7 +44,7 @@ class TestFileHeaders(object):
         header = """# coding=utf-8
 # --------------------------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See License.txt in the project root for license information.
+# PRIVATE DISTRIBUTION FOR NDA CUSTOMERS ONLY
 # --------------------------------------------------------------------------------------------"""
 
         files_missing_header = []
