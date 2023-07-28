09c37c795ae58cde0014e3d0a2721c368a1a5e58
Paymaun Heidari <digimaun@users.noreply.github.com>
2023-07-28 15:24:30 -0700

    Add symphony compatible API in support bundle help

--- azext_edge/edge/_help.py
+++ azext_edge/edge/_help.py
@@ -9,7 +9,7 @@ Help definitions for Digital Twins commands.
 
 from knack.help_files import helps
 from .providers.edge_api import E4K_ACTIVE_API
-from .providers.support_bundle import COMPAT_BLUEFIN_APIS, COMPAT_E4K_APIS, COMPAT_OPCUA_APIS
+from .providers.support_bundle import COMPAT_BLUEFIN_APIS, COMPAT_E4K_APIS, COMPAT_OPCUA_APIS, COMPAT_SYMPHONY_APIS
 
 
 def load_iotedge_help():
@@ -42,6 +42,7 @@ def load_iotedge_help():
                 {COMPAT_E4K_APIS.as_str()}
                 {COMPAT_OPCUA_APIS.as_str()}
                 {COMPAT_BLUEFIN_APIS.as_str()}
+                {COMPAT_SYMPHONY_APIS.as_str()}
     """
 
     helps[
