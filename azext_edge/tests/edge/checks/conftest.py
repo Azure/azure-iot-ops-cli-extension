2f85e10d3ce9a5a59df418aecf82874ddfce4a19
Ryan K <ryan.k@outlook.com>
2023-09-12 08:59:45 -0700

    WIP tests

---
+++ azext_edge/tests/edge/checks/conftest.py
@@ -0,0 +1,35 @@
+# coding=utf-8
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Private distribution for NDA customers only. Governed by license terms at https://preview.e4k.dev/docs/use-terms/
+# --------------------------------------------------------------------------------------------
+
+from functools import partial
+from io import BufferedReader
+from pathlib import PurePath
+from typing import List
+
+import pytest
+
+from ...generators import generate_generic_id
+
+@pytest.fixture
+def mock_evaluate_pod_health(mocker):
+    patched = mocker.patch("azext_edge.edge.providers.checks.evaluate_pod_health", return_value={})
+    yield patched
+
+# test_checks_unit::test_check_by_resource_types
+@pytest.fixture
+def mock_e4k_resource_types(mocker):
+    patched = mocker.patch("azext_edge.edge.providers.checks.enumerate_e4k_resources")
+    patched.return_value = (
+        {},
+        {
+            "Broker": [{}],
+            "BrokerListener": [{}],
+            "DiagnosticService": [{}],
+            "MqttBridgeConnector": [{}],
+            "DataLakeConnector": [{}]
+        }
+    )
+    yield patched
--- azext_edge/tests/edge/checks/test_checks_unit.py
+++ azext_edge/tests/edge/checks/test_checks_unit.py
@@ -5,10 +5,10 @@
 # --------------------------------------------------------------------------------------------
 
 
-from azext_edge.edge.providers.checks import CheckManager
+from azext_edge.edge.providers.checks import CheckManager, run_checks, E4kResourceKinds, ResourceState
 from azext_edge.edge.common import CheckTaskStatus
 from ...generators import generate_generic_id
-
+import pytest
 
 def test_check_manager():
     name = generate_generic_id()
@@ -138,3 +138,139 @@ def assert_check_manager_dict(
         result_check_dict_displays = check_manager.as_dict(as_list=True)
         for target in expected_target_displays:
             assert expected_target_displays[target] == result_check_dict_displays["targets"][target]["displays"]
+
+@pytest.mark.parametrize(
+    "resource_kinds",
+    [
+        None,
+        [],
+        [
+            E4kResourceKinds.BROKER.value
+        ],
+        [
+            E4kResourceKinds.BROKER.value,
+            E4kResourceKinds.BROKER_LISTENER.value
+        ],
+        [
+            E4kResourceKinds.DIAGNOSTIC_SERVICE.value
+        ],
+        [
+            E4kResourceKinds.MQTT_BRIDGE_CONNECTOR.value,
+            E4kResourceKinds.DATALAKE_CONNECTOR.value,
+        ],
+        [
+            E4kResourceKinds.BROKER.value,
+            E4kResourceKinds.BROKER_LISTENER.value,
+            E4kResourceKinds.DIAGNOSTIC_SERVICE.value,
+            E4kResourceKinds.MQTT_BRIDGE_CONNECTOR.value,
+            E4kResourceKinds.DATALAKE_CONNECTOR.value,
+        ],
+    ],
+)
+def test_check_by_resource_types(mocker, mock_e4k_resource_types, resource_kinds):
+    eval_lookup = {
+        E4kResourceKinds.BROKER.value: mocker.patch(
+            "azext_edge.edge.providers.checks.evaluate_brokers", return_value={}
+        ),
+        E4kResourceKinds.BROKER_LISTENER.value: mocker.patch(
+            "azext_edge.edge.providers.checks.evaluate_broker_listeners",
+            return_value={},
+        ),
+        E4kResourceKinds.DIAGNOSTIC_SERVICE.value: mocker.patch(
+            "azext_edge.edge.providers.checks.evaluate_diagnostics_service",
+            return_value={},
+        ),
+        E4kResourceKinds.MQTT_BRIDGE_CONNECTOR.value: mocker.patch(
+            "azext_edge.edge.providers.checks.evaluate_mqtt_bridge_connectors",
+            return_value={},
+        ),
+        E4kResourceKinds.DATALAKE_CONNECTOR.value: mocker.patch(
+            "azext_edge.edge.providers.checks.evaluate_datalake_connectors",
+            return_value={},
+        ),
+    }
+
+    # run the checks
+    run_checks(
+        namespace='default',
+        pre_deployment=False,
+        post_deployment=True,
+        as_list=False,
+        resource_kinds=resource_kinds,
+    )
+
+    if not resource_kinds:
+        # ensure all checks were run
+        [eval_lookup[evaluator].assert_called_once() for evaluator in eval_lookup]
+    else:
+        # ensure each individual resource kind check was run once
+        for resource_kind in resource_kinds:
+            eval_lookup[resource_kind].assert_called_once()
+            del eval_lookup[resource_kind]
+        # ensure no other checks were run
+        [eval_lookup[evaluator].assert_not_called() for evaluator in eval_lookup]
+
+@pytest.mark.parametrize(
+    "mock_resource_items, assertions",
+    [
+        (
+            # broker items
+            [
+                {
+                    "metadata": {"namespace": "mock_namespace", "name": "mock_name"},
+                    "spec": {
+                        "diagnostics": {
+                            "diagnosticServiceEndpoint": "None",
+                            "enableMetrics": "True",
+                            "enableSelfCheck": "True",
+                            "enableSelfTracing": "True",
+                            "enableTracing": "True",
+                            "logFormat": "text",
+                            "logLevel": "info,hyper=off,kube_client=off,tower=off,conhash=off,h2=off",
+                        },
+                        "cardinality": {
+                            "backendChain": { "partitions": 1, "replicas": 2, "workers": 1 },
+                            "frontend": { "replicas": 1 }
+                        },
+                    },
+                    "status": {
+                        "status": ResourceState.running.value,
+                        "statusDescription": ""
+                    }
+                }
+            ],
+            # assertions
+            {
+                'test': 'test'
+            }
+        )
+    ]
+)
+def test_broker_checks(
+    mocker,
+    mock_evaluate_pod_health,
+    mock_resource_items,
+    assertions
+):
+    # from unittest.mock import MagicMock
+    from azext_edge.edge.providers.checks import evaluate_brokers
+    
+    # broker_list: dict = E4K_ACTIVE_API.get_resources(E4kResourceKinds.BROKER, namespace=namespace)
+    mocker.patch('azext_edge.edge.providers.edge_api.base.EdgeResourceApi.get_resources', return_value={
+        "items": mock_resource_items
+    })
+
+    namespace = generate_generic_id()
+    result = evaluate_brokers(namespace=namespace)
+
+def test_broker_listner_checks():
+    pass
+
+def test_diagnostic_service_checks():
+    pass
+
+def test_mqtt_checks():
+    pass
+
+def test_datalake_checks():
+    pass
