857dd9b45b8c3272d4399c6ca1c1a927bfc61b20
Victoria Litvinova <vilit@microsoft.com>
2023-09-18 16:09:42 -0700

    copy embedded test

--- azext_edge/tests/utility/test_iot_utility_unit.py
+++ azext_edge/tests/utility/test_iot_utility_unit.py
@@ -4,8 +4,12 @@
 # Private distribution for NDA customers only. Governed by license terms at https://preview.e4k.dev/docs/use-terms/
 # --------------------------------------------------------------------------------------------
 
+import json
 import os
 import pytest
+from knack.util import CLIError
+from azure.cli.core.azclierror import CLIInternalError
+from ...common.embedded_cli import EmbeddedCLI
 
 
 class TestCliInit(object):
@@ -68,3 +72,79 @@ class TestFileHeaders(object):
                 "The following files are missing an encoding and license header, or it is improperly formatted:\n"
                 "{}".format("\n".join(files_missing_header))
             )
+
+
+class TestEmbeddedCli(object):
+    @pytest.fixture(params=[0, 1, 2])
+    def mocked_azclient(self, mocker, request):
+        azclient = mocker.patch("azext_iot.common.embedded_cli.get_default_cli")
+
+        def mock_invoke(args, out_file):
+            azclient.return_value.exception_handler("Generic Issue")
+            azclient.return_value.result.error = None
+            if request.param == 0:
+                out_file.write(json.dumps({"generickey": "genericvalue"}))
+            else:
+                out_file.write("Something not json")
+                if request.param == 1:
+                    azclient.return_value.result.error = CLIError("Generic Error")
+
+            return request.param
+
+        azclient.return_value.invoke.side_effect = mock_invoke
+        azclient.test_meta.error_code = request.param
+        return azclient
+
+    @pytest.mark.parametrize("command", [
+        "iot hub device-identity create -n abcd -d dcba",
+        "iot hub device-twin show -n 'abcd' -d 'dcba'"
+    ])
+    @pytest.mark.parametrize("user_subscription", [None, "20a300e5-a444-4130-bb5a-1abd08ad930a"])
+    @pytest.mark.parametrize("subscription", [None, "40a300e5-4130-a444-bb5a-1abd08ad930a"])
+    @pytest.mark.parametrize("init_capture_stderr", [True, False])
+    @pytest.mark.parametrize("capture_stderr", [None, True, False])
+    def test_embedded_cli(
+        self, mocker, mocked_azclient, command, user_subscription, subscription, init_capture_stderr, capture_stderr
+    ):
+        import shlex
+
+        cli_ctx = mocker.MagicMock()
+        cli_ctx.data = {}
+        if user_subscription:
+            cli_ctx.data["subscription_id"] = user_subscription
+
+        expected_count = 0 if (capture_stderr is None and init_capture_stderr) or capture_stderr else 1
+        cli = EmbeddedCLI(cli_ctx, capture_stderr=init_capture_stderr)
+
+        if mocked_azclient.test_meta.error_code != 1 or expected_count == 1:
+            cli.invoke(command=command, subscription=subscription, capture_stderr=capture_stderr)
+        else:
+            with pytest.raises(CLIError) as e:
+                cli.invoke(command=command, subscription=subscription, capture_stderr=capture_stderr)
+            assert "Generic Error" in str(e.value)
+
+        assert cli.az_cli.exception_handler.call_count == expected_count
+
+        # Due to forced json output
+        command += " -o json"
+
+        if subscription:
+            command += " --subscription '{}'".format(subscription)
+        elif user_subscription:
+            command += " --subscription '{}'".format(user_subscription)
+
+        expected_args = shlex.split(command)
+        call = mocked_azclient().invoke.call_args_list[0]
+        actual_args, _ = call
+        assert expected_args == actual_args[0]
+        assert cli.output
+        success = cli.success()
+        if mocked_azclient.test_meta.error_code > 0:
+            assert not success
+            if mocked_azclient.test_meta.error_code == 2:
+                with pytest.raises(CLIInternalError) as e:
+                    cli.as_json()
+                assert "Issue parsing received payload" in str(e.value)
+        elif mocked_azclient.test_meta.error_code == 0:
+            assert success
+            assert cli.as_json()
