5064f7886a95d1e75caf120e55d3398736a04407
Paymaun <digimaun@users.noreply.github.com>
2023-10-02 15:42:57 -0700

    chore: increment to e4k 0.6.0 (#35)
    * Increment PAS 0.1.2 version def.
    * Increment CLI version.
    * Increment az-e4k helm chart.

--- azext_edge/constants.py
+++ azext_edge/constants.py
@@ -7,7 +7,7 @@
 
 import os
 
-VERSION = "0.0.4a1.dev2"
+VERSION = "0.0.4a1"
 EXTENSION_NAME = "azure-edge"
 EXTENSION_ROOT = os.path.dirname(os.path.abspath(__file__))
 USER_AGENT = "EdgePlatformCliExtension/{}".format(VERSION)
