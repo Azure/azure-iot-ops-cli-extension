b109afac3e98f36c6d6a62d1cc36463690230903
Ryan K <ryan.k@outlook.com>
2023-10-02 22:09:23 +0000

    Devcontainer host requirements and post-create updates
    Added host requirements for 4 cpus, 8gb memory, 32gb storage
    
    explicitly write kubeconfig and install local extension to default directory

--- .devcontainer/post_create_commands.sh
+++ .devcontainer/post_create_commands.sh
@@ -1,5 +1,18 @@
 # create local k3s cluster
 k3d cluster create -i ghcr.io/jlian/k3d-nfs:v1.25.3-k3s1
 
+# write kubeconfig
+k3d kubeconfig get k3s-default > ~/.kube/config
+
 # install connectedk8s extension
 az extension add -n connectedk8s -y
+
+# local extension install
+pip install -U --target ~/.azure/cliextensions/azure-edge .
+
+# Run the following to connect your cluster to ARC and install PAS
+# RESOURCE_GROUP=[your_cluster_resource_group]
+# az login [--use-device-code]
+# az connectedk8s connect -n $CODESPACE_NAME -g $RESOURCE_GROUP
+# az connectedk8s enable-features -n $CODESPACE_NAME -g $RESOURCE_GROUP --features cluster-connect custom-locations
+# az edge init --cluster $CODESPACE_NAME -g $RESOURCE_GROUP [--create-sync-rules]
