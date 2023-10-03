5539d8ea8f9ad98c4851e64316b998f360273a37
Ryan K <ryan.k@outlook.com>
2023-10-03 16:50:12 +0000

    updates to devcontainer
    install script now creates virtualenv and installs CLI edge
    
    devcontainer contains python extension and hopefully sets venv correctly on launch

--- .devcontainer/post_create_commands.sh
+++ .devcontainer/post_create_commands.sh
@@ -1,4 +1,5 @@
 # create local k3s cluster
+echo "Creating k3d cluster"
 k3d cluster create -i ghcr.io/jlian/k3d-nfs:v1.25.3-k3s1
 
 # write kubeconfig
@@ -8,7 +9,21 @@ k3d kubeconfig get k3s-default > ~/.kube/config
 az extension add -n connectedk8s -y
 
 # local extension install
+echo "Setting up CLI dev environment"
+python -m venv env
+source env/bin/activate
+
+echo "Install AZDEV"
+pip install azdev
+
+echo "Install AZ CLI EDGE"
+azdev setup -c EDGE
+
+echo "Installing local dev extension"
 pip install -U --target ~/.azure/cliextensions/azure-edge .
+
+echo "Install complete, please activate your environment with 'source env/bin/activate'"
+echo "This should automatically occur the next time you connect to the codespace"
 
 # Run the following to connect your cluster to ARC and install PAS
 # RESOURCE_GROUP=[your_cluster_resource_group]
