babc62b6639614a8343a7f5a5d9797d7754338ed
Ryan K <ryan.k@outlook.com>
2023-10-03 19:16:03 +0000

    Added port forwarding to cluster create

--- .devcontainer/post_create_commands.sh
+++ .devcontainer/post_create_commands.sh
@@ -1,6 +1,10 @@
 # create local k3s cluster
 echo "Creating k3d cluster"
-k3d cluster create -i ghcr.io/jlian/k3d-nfs:v1.25.3-k3s1
+k3d cluster create -i ghcr.io/jlian/k3d-nfs:v1.25.3-k3s1 \
+-p '1883:1883@loadbalancer' \
+-p '8883:8883@loadbalancer' \
+-p '6001:6001@loadbalancer' \
+-p '4000:80@loadbalancer'
 
 # write kubeconfig
 k3d kubeconfig get k3s-default > ~/.kube/config
@@ -28,6 +32,10 @@ echo "This should automatically occur the next time you connect to the codespace
 # Run the following to connect your cluster to ARC and install PAS
 # RESOURCE_GROUP=[your_cluster_resource_group]
 # az login [--use-device-code]
+#
+# If you've rebuilt this codespace, you'll also need to run the following command before running `az connectedk8s connect`:
+# az connectedk8s delete -n $CODESPACE_NAME -g $RESOURCE_GROUP
+#
 # az connectedk8s connect -n $CODESPACE_NAME -g $RESOURCE_GROUP
 # az connectedk8s enable-features -n $CODESPACE_NAME -g $RESOURCE_GROUP --features cluster-connect custom-locations
 # az edge init --cluster $CODESPACE_NAME -g $RESOURCE_GROUP [--create-sync-rules]
