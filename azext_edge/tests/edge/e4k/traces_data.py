7277d68afc575087b08b187522d6f57363603b07
Paymaun Heidari <digimaun@users.noreply.github.com>
2023-10-10 20:27:48 -0700

    Exclude test file from lint.

--- azext_edge/tests/edge/e4k/traces_data.py
+++ azext_edge/tests/edge/e4k/traces_data.py
@@ -3,6 +3,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Private distribution for NDA customers only. Governed by license terms at https://preview.e4k.dev/docs/use-terms/
 # --------------------------------------------------------------------------------------------
+# flake8: noqa
 
 TEST_TRACES_DATA = {
     "resourceSpans": [
