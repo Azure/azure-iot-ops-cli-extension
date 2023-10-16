1aa6c4401656a0979dc55f2e1fa2a2deb0a7be2b
Paymaun Heidari <digimaun@users.noreply.github.com>
2023-10-16 11:45:40 -0700

    Update version for release.

--- azext_edge/constants.py
+++ azext_edge/constants.py
@@ -7,7 +7,7 @@
 
 import os
 
-VERSION = "0.0.4a2.dev1"
+VERSION = "0.0.4a2"
 EXTENSION_NAME = "azure-edge"
 EXTENSION_ROOT = os.path.dirname(os.path.abspath(__file__))
 USER_AGENT = "EdgePlatformCliExtension/{}".format(VERSION)
