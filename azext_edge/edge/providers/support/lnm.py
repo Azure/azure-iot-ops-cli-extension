3867128c5f1f69254829fa2a66195d80562e31d1
Elsie4ever <3467996@gmail.com>
2023-10-24 09:04:59 -0700

    add header to suport/lnm.py

--- azext_edge/edge/providers/support/lnm.py
+++ azext_edge/edge/providers/support/lnm.py
@@ -1,3 +1,9 @@
+# coding=utf-8
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Private distribution for NDA customers only. Governed by license terms at https://preview.e4k.dev/docs/use-terms/
+# --------------------------------------------------------------------------------------------
+
 from typing import Iterable, List
 
 from functools import partial
