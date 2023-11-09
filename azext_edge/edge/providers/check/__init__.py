d42b84d6c941f82034abbe0603ba4a4bb3c3ceae
Paymaun Heidari <digimaun@users.noreply.github.com>
2023-11-08 18:04:45 -0800

    License headers

--- azext_edge/edge/providers/check/__init__.py
+++ azext_edge/edge/providers/check/__init__.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-# --------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
-# Private distribution for NDA customers only. Governed by license terms at https://preview.e4k.dev/docs/use-terms/
-# --------------------------------------------------------------------------------------------
+# Licensed under the MIT License. See License file in the project root for license information.
+# ----------------------------------------------------------------------------------------------
