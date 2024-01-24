b29fd46a3d207d597c0da172600f8e112b65cbd6
Paymaun <digimaun@users.noreply.github.com>
2024-01-24 10:32:57 -0800

    fix: improve error mode when CSI driver deployment mismatches kubeconfig cluster (#129)

--- azext_edge/edge/providers/orchestration/work.py
+++ azext_edge/edge/providers/orchestration/work.py
@@ -6,8 +6,9 @@
 
 from enum import IntEnum
 from time import sleep
-from typing import Dict, Tuple
+from typing import Dict, Tuple, Union
 from uuid import uuid4
+from json import dumps
 
 from azure.cli.core.azclierror import AzureResponseError, ValidationError
 from azure.core.exceptions import HttpResponseError
@@ -54,6 +55,8 @@ class WorkRecord:
 
 CLUSTER_SECRET_REF = "aio-akv-sp"
 CLUSTER_SECRET_CLASS_NAME = "aio-default-spc"
+
+PRE_FLIGHT_SUCCESS_STATUS = "succeeded"
 
 
 class WorkDisplay:
@@ -228,6 +231,9 @@ class WorkManager:
                     **self._kwargs,
                 )
                 terminal_deployment = wait_for_terminal_state(deployment_poller)
+                pre_flight_result: Dict[str, Union[dict, str]] = terminal_deployment.as_dict()
+                if "status" in pre_flight_result and pre_flight_result["status"].lower() != PRE_FLIGHT_SUCCESS_STATUS:
+                    raise AzureResponseError(dumps(pre_flight_result, indent=2))
 
                 self._completed_steps[WorkStepKey.EVAL_LOGIN_PERM] = 1
                 self.render_display(WorkCategoryKey.PRE_CHECK)
