2fd8558982338b176edec9cd58bb705292f5f34b
Victoria Litvinova <73560279+vilit1@users.noreply.github.com>
2024-02-29 14:46:45 -0800

    fix: update akri labels for `az iot ops support create-bundle` (#153)

--- azext_edge/tests/edge/support/test_support_unit.py
+++ azext_edge/tests/edge/support/test_support_unit.py
@@ -25,7 +25,7 @@ from azext_edge.edge.providers.edge_api import (
     ORC_API_V1,
     EdgeResourceApi,
 )
-from azext_edge.edge.providers.support.akri import AKRI_NAME_LABEL, AKRI_SERVICE_LABEL
+from azext_edge.edge.providers.support.akri import AKRI_APP_LABEL, AKRI_INSTANCE_LABEL, AKRI_SERVICE_LABEL
 from azext_edge.edge.providers.support.base import get_bundle_path
 from azext_edge.edge.providers.support.dataprocessor import (
     DATA_PROCESSOR_INSTANCE_LABEL,
@@ -49,7 +49,6 @@ from azext_edge.edge.providers.support.orc import (
 from azext_edge.edge.providers.support_bundle import COMPAT_MQ_APIS
 
 from ...generators import generate_generic_id
-from .conftest import add_pod_to_mocked_pods
 
 a_bundle_dir = f"support_test_{generate_generic_id()}"
 
@@ -96,10 +95,6 @@ def test_create_bundle(
         mocked_root_logger.warning.assert_called_once_with("No known IoT Operations services discovered on cluster.")
         assert auto_result_no_resources is None
         return
-
-    # @vilit - AKRI pod with ambigious label
-    if AKRI_API_V0 in mocked_cluster_resources["param"]:
-        add_pod_to_mocked_pods(mocked_client, mocked_list_pods, ["aio-akri-otel-collector"])
 
     since_seconds = random.randint(86400, 172800)
     result = support_bundle(None, bundle_dir=a_bundle_dir, log_age_seconds=since_seconds)
@@ -293,7 +288,7 @@ def test_create_bundle(
                 mocked_client,
                 mocked_zipfile,
                 mocked_list_pods,
-                label_selector=AKRI_NAME_LABEL,
+                label_selector=AKRI_INSTANCE_LABEL,
                 resource_api=AKRI_API_V0,
                 since_seconds=since_seconds,
             )
@@ -301,34 +296,45 @@ def test_create_bundle(
                 mocked_client,
                 mocked_zipfile,
                 mocked_list_pods,
-                label_selector=None,
+                label_selector=AKRI_APP_LABEL,
                 resource_api=AKRI_API_V0,
                 since_seconds=since_seconds,
-                mock_names=["aio-akri-otel-collector"],
             )
             assert_list_deployments(
                 mocked_client,
                 mocked_zipfile,
-                label_selector=None,
+                label_selector=AKRI_INSTANCE_LABEL,
                 resource_api=AKRI_API_V0,
-                mock_names=["aio-akri-otel-collector"],
+            )
+            assert_list_deployments(
+                mocked_client,
+                mocked_zipfile,
+                label_selector=AKRI_APP_LABEL,
+                resource_api=AKRI_API_V0,
             )
             assert_list_replica_sets(
                 mocked_client,
                 mocked_zipfile,
-                label_selector=None,
+                label_selector=AKRI_INSTANCE_LABEL,
                 resource_api=AKRI_API_V0,
-                mock_names=["aio-akri-otel-collector"],
+            )
+            assert_list_replica_sets(
+                mocked_client,
+                mocked_zipfile,
+                label_selector=AKRI_APP_LABEL,
+                resource_api=AKRI_API_V0,
             )
             assert_list_services(
                 mocked_client, mocked_zipfile, label_selector=AKRI_SERVICE_LABEL, resource_api=AKRI_API_V0
             )
+            assert_list_services(
+                mocked_client, mocked_zipfile, label_selector=AKRI_INSTANCE_LABEL, resource_api=AKRI_API_V0
+            )
             assert_list_daemon_sets(
                 mocked_client,
                 mocked_zipfile,
-                label_selector=None,
+                label_selector=AKRI_INSTANCE_LABEL,
                 resource_api=AKRI_API_V0,
-                mock_names=["aio-akri-agent-daemonset"],
             )
 
         if api in [LNM_API_V1B1]:
