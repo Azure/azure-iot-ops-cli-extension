f9e5e401fa6e9978c8d13265fbff79f031ac5f5f
Paymaun Heidari <digimaun@users.noreply.github.com>
2024-04-23 13:12:15 -0700

    WIP

--- azext_edge/constants.py
+++ azext_edge/constants.py
@@ -7,7 +7,7 @@
 
 import os
 
-VERSION = "0.4.0b5"
+VERSION = "0.4.0b5.dev1"
 EXTENSION_NAME = "azure-iot-ops"
 EXTENSION_ROOT = os.path.dirname(os.path.abspath(__file__))
 USER_AGENT = "IotOperationsCliExtension/{}".format(VERSION)
