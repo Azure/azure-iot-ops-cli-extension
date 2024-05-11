59e0dacc74a13cbda47e41975360c085bc3b6355
Paymaun Heidari <digimaun@users.noreply.github.com>
2024-05-11 12:18:00 -0700

    WIP

--- azext_edge/tests/edge/orchestration/test_resource_map_unit.py
+++ azext_edge/tests/edge/orchestration/test_resource_map_unit.py
@@ -6,6 +6,7 @@
 
 from typing import List, Optional
 from unittest.mock import Mock
+from random import randint
 
 import pytest
 from rich.tree import Tree
@@ -23,8 +24,20 @@ def mocked_connected_cluster(mocker):
     yield patched
 
 
-def _generate_records() -> List[dict]:
-    return [{"id": generate_random_string(), "name": generate_random_string(), "apiVersion": generate_random_string()}]
+def _generate_records(count: int = 1) -> List[dict]:
+    result = []
+
+    for i in range(count):
+        high = randint(5, 10)
+        low = randint(1, 4)
+        segments = high if i % 2 == 0 else low
+
+        resource_id = ""
+        for _ in range(segments):
+            resource_id = f"{resource_id}/{generate_random_string()}"
+        result.append({"id": resource_id, "name": resource_id.split("/")[-1], "apiVersion": generate_random_string()})
+
+    return result
 
 
 def _assemble_connected_cluster_mock(
@@ -41,22 +54,22 @@ def _assemble_connected_cluster_mock(
 
 
 @pytest.mark.parametrize(
-    "expected_aio_extensions",
+    "expected_extensions",
     [None, _generate_records()],
 )
 @pytest.mark.parametrize(
-    "expected_aio_custom_locations",
+    "expected_custom_locations",
     [None, _generate_records()],
 )
-@pytest.mark.parametrize("expected_aio_resources", [None, _generate_records()])
+@pytest.mark.parametrize("expected_resources", [None, _generate_records(5)])
 @pytest.mark.parametrize("expected_resource_sync_rules", [None, _generate_records()])
 def test_resource_map(
     mocker,
     mocked_cmd: Mock,
     mocked_connected_cluster: Mock,
-    expected_aio_extensions: Optional[List[dict]],
-    expected_aio_custom_locations: Optional[List[dict]],
-    expected_aio_resources: Optional[List[dict]],
+    expected_extensions: Optional[List[dict]],
+    expected_custom_locations: Optional[List[dict]],
+    expected_resources: Optional[List[dict]],
     expected_resource_sync_rules: Optional[List[dict]],
 ):
     from azext_edge.edge.providers.orchestration.resource_map import (
@@ -65,9 +78,9 @@ def test_resource_map(
 
     _assemble_connected_cluster_mock(
         cluster_mock=mocked_connected_cluster,
-        extensions=expected_aio_extensions,
-        custom_locations=expected_aio_custom_locations,
-        resources=expected_aio_resources,
+        extensions=expected_extensions,
+        custom_locations=expected_custom_locations,
+        resources=expected_resources,
         sync_rules=expected_resource_sync_rules,
     )
 
@@ -80,31 +93,47 @@ def test_resource_map(
     assert resource_map.subscription_id == sub
 
     mocked_connected_cluster().get_aio_extensions.assert_called_once()
-    _assert_ops_resource_eq(resource_map.extensions, expected_aio_extensions)
+    _assert_ops_resource_eq(resource_map.extensions, expected_extensions)
 
     mocked_connected_cluster().get_aio_custom_locations.assert_called_once()
-    _assert_ops_resource_eq(resource_map.custom_locations, expected_aio_custom_locations)
+    _assert_ops_resource_eq(resource_map.custom_locations, expected_custom_locations)
 
-    cl_count = 0 if not expected_aio_custom_locations else len(expected_aio_custom_locations)
+    cl_count = 0 if not expected_custom_locations else len(expected_custom_locations)
     assert mocked_connected_cluster().get_aio_resources.call_count == cl_count
     assert mocked_connected_cluster().get_resource_sync_rules.call_count == cl_count
 
-    if expected_aio_custom_locations:
-        for cl in expected_aio_custom_locations:
-            _assert_ops_resource_eq(resource_map.get_resources(cl["id"]), expected_aio_resources)
+    if expected_custom_locations:
+        for cl in expected_custom_locations:
+            _assert_ops_resource_eq(
+                resource_map.get_resources(cl["id"]), expected_resources, verify_segment_order=True
+            )
             _assert_ops_resource_eq(resource_map.get_resource_sync_rules(cl["id"]), expected_resource_sync_rules)
 
     _assert_tree(
         resource_map.build_tree(),
         cluster_name=cluster_name,
-        expected_aio_extensions=expected_aio_extensions,
-        expected_aio_custom_locations=expected_aio_custom_locations,
-        expected_aio_resources=expected_aio_resources,
+        expected_aio_extensions=expected_extensions,
+        expected_aio_custom_locations=expected_custom_locations,
+        expected_aio_resources=expected_resources,
         expected_resource_sync_rules=expected_resource_sync_rules,
     )
 
 
-def _assert_ops_resource_eq(actual: List[IoTOperationsResource], expected: List[dict]):
+def _assert_ops_resource_eq(
+    actual: List[IoTOperationsResource], expected: List[dict], verify_segment_order: bool = False
+):
+    if verify_segment_order and actual:
+        last_segments: int = 999
+        ids = {record["id"] for record in expected}
+        for i in range(len(actual)):
+            assert isinstance(actual[i].segments, int)
+            assert actual[i].segments <= last_segments
+            last_segments = actual[i].segments
+            # Serves as membership assertion, removing non-member throws KeyError
+            ids.remove(actual[i].resource_id)
+        assert not ids
+        return
+
     for i in range(len(actual)):
         assert actual[i].resource_id == expected[i]["id"]
         assert actual[i].display_name == expected[i]["name"]
