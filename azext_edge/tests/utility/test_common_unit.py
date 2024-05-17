6dccbbd33d452992ce31ca76455faecc9df87395
Paymaun Heidari <digimaun@users.noreply.github.com>
2024-05-16 17:20:32 -0700

    lint

--- azext_edge/tests/utility/test_common_unit.py
+++ azext_edge/tests/utility/test_common_unit.py
@@ -12,8 +12,7 @@ from azext_edge.edge.util import url_safe_random_chars
 @pytest.mark.parametrize("expected_count", [10, 5, 0, -1])
 def test_url_safe_random_chars(expected_count: int):
     result = url_safe_random_chars(expected_count)
-    if expected_count < 0:
-        expected_count = 0
+    expected_count = max(expected_count, 0)
     assert len(result) == expected_count
 
     expected_char_set = string.ascii_lowercase + string.ascii_uppercase + string.digits
