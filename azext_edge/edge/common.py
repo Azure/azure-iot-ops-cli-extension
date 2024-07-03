56e8d89812e3ac50f75975fa54ecbfaea7b19509
Ryan Kelly <ryan@UPX-M2.local>
2024-07-02 21:46:58 -0700

    fixed hanging dp ref

--- azext_edge/edge/common.py
+++ azext_edge/edge/common.py
@@ -153,7 +153,6 @@ class OpsServiceType(ListableEnum):
         return [
             cls.mq.value,
             cls.opcua.value,
-            cls.dataprocessor.value,
             cls.akri.value,
             cls.deviceregistry.value,
         ]
