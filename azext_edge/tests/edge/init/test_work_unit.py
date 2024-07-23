0361ae2ee093f823804e9c91ab20725de733e5f8
Paymaun Heidari <digimaun@users.noreply.github.com>
2024-07-22 18:41:31 -0700

    WIP

--- azext_edge/tests/edge/init/test_work_unit.py
+++ azext_edge/tests/edge/init/test_work_unit.py
@@ -259,7 +259,7 @@ def test_init_to_template_params(
     if instance_name:
         assert parameters["instanceName"]["value"] == instance_name
     else:
-        assert parameters["instanceName"]["value"] == f"{lowered_cluster_name}-ops-init-instance"
+        assert parameters["instanceName"]["value"] == f"{lowered_cluster_name}-ops-instance"
 
     assert parameters["clusterLocation"]["value"] == connected_cluster_location
     if location:
@@ -277,7 +277,7 @@ def test_init_to_template_params(
         expected_char_set = string.ascii_lowercase + string.ascii_uppercase + string.digits
         for c in split_custom_location_name[1]:
             assert c in expected_char_set
-        parameters["customLocationName"]["value"].endswith("-ops-init-cli")
+        assert parameters["customLocationName"]["value"].endswith("-ops-init-cl")
 
     if simulate_plc:
         assert "simulatePLC" in parameters and parameters["simulatePLC"]["value"] is True
