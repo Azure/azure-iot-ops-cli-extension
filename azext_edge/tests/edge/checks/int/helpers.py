60796214d487bb95152788b84c315615f9990a1b
Elsie4ever <3467996@gmail.com>
2024-07-23 17:54:33 -0700

    update

--- azext_edge/tests/edge/checks/int/helpers.py
+++ azext_edge/tests/edge/checks/int/helpers.py
@@ -159,7 +159,7 @@ def assert_general_eval_custom_resources(
     assert post_deployment[key]
     assert post_deployment[key]["description"].startswith(f"Evaluate {description_name}")
     # for the ones that have spaces
-    assert post_deployment[key]["description"].replace(" ", "").lower().endswith(resource_plural)
+    assert post_deployment[key]["description"].replace(" ", "").lower().endswith(resource_plural.lower())
 
     # check the target existence
     sorted_items = sort_kubectl_items_by_namespace(items, include_all=include_all_namespace)
