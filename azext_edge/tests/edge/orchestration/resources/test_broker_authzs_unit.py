28849ac17f30d080db810d2798cca5bb31f1b19f
Paymaun Heidari <digimaun@users.noreply.github.com>
2024-07-25 18:10:00 -0700

    WIP

--- azext_edge/tests/edge/orchestration/resources/test_broker_authzs_unit.py
+++ azext_edge/tests/edge/orchestration/resources/test_broker_authzs_unit.py
@@ -9,43 +9,48 @@ from typing import Optional
 import pytest
 import responses
 
-from azext_edge.edge.commands_mq import show_broker_authn, list_broker_authns
+from azext_edge.edge.commands_mq import show_broker_authz, list_broker_authzs
 
 from ....generators import generate_random_string
 from .conftest import get_base_endpoint, get_mock_resource
 
 
-def get_broker_authn_endpoint(
-    instance_name: str, broker_name: str, resource_group_name: str, authn_name: Optional[str] = None
+def get_broker_authz_endpoint(
+    instance_name: str, broker_name: str, resource_group_name: str, authz_name: Optional[str] = None
 ):
-    resource_path = f"/instances/{instance_name}/brokers/{broker_name}/authentications"
-    if authn_name:
-        resource_path += f"/{authn_name}"
+    resource_path = f"/instances/{instance_name}/brokers/{broker_name}/authorizations"
+    if authz_name:
+        resource_path += f"/{authz_name}"
     return get_base_endpoint(resource_group_name=resource_group_name, resource_path=resource_path)
 
 
-def get_mock_broker_authn_record(authn_name: str, broker_name: str, instance_name: str, resource_group_name: str):
+def get_mock_broker_authz_record(authz_name: str, broker_name: str, instance_name: str, resource_group_name: str):
     return get_mock_resource(
-        name=authn_name,
-        resource_path=f"/instances/{instance_name}/brokers/{broker_name}/authentications/{authn_name}",
+        name=authz_name,
+        resource_path=f"/instances/{instance_name}/brokers/{broker_name}/authorizations/{authz_name}",
         properties={
-            "authenticationMethods": [
-                {"method": "ServiceAccountToken", "serviceAccountToken": {"audiences": ["aio-mq"]}}
-            ],
+            "authorizationPolicies": {
+                "cache": "Disabled",
+                "rules": {
+                    "brokerResources": [{"method": "Connect", "topics": ["*"]}],
+                    "principals": {"attributes": [], "clientIds": [], "usernames": []},
+                    "stateStoreResources": [],
+                },
+            },
             "provisioningState": "Succeeded",
         },
         resource_group_name=resource_group_name,
     )
 
 
-def test_broker_authn_show(mocked_cmd, mocked_responses: responses):
-    authn_name = generate_random_string()
+def test_broker_authz_show(mocked_cmd, mocked_responses: responses):
+    authz_name = generate_random_string()
     broker_name = generate_random_string()
     instance_name = generate_random_string()
     resource_group_name = generate_random_string()
 
-    mock_broker_authn_record = get_mock_broker_authn_record(
-        authn_name=authn_name,
+    mock_broker_authz_record = get_mock_broker_authz_record(
+        authz_name=authz_name,
         broker_name=broker_name,
         instance_name=instance_name,
         resource_group_name=resource_group_name,
@@ -53,26 +58,26 @@ def test_broker_authn_show(mocked_cmd, mocked_responses: responses):
 
     mocked_responses.add(
         method=responses.GET,
-        url=get_broker_authn_endpoint(
+        url=get_broker_authz_endpoint(
             resource_group_name=resource_group_name,
             instance_name=instance_name,
             broker_name=broker_name,
-            authn_name=authn_name,
+            authz_name=authz_name,
         ),
-        json=mock_broker_authn_record,
+        json=mock_broker_authz_record,
         status=200,
         content_type="application/json",
     )
 
-    result = show_broker_authn(
+    result = show_broker_authz(
         cmd=mocked_cmd,
-        authn_name=authn_name,
+        authz_name=authz_name,
         mq_broker_name=broker_name,
         instance_name=instance_name,
         resource_group_name=resource_group_name,
     )
 
-    assert result == mock_broker_authn_record
+    assert result == mock_broker_authz_record
     assert len(mocked_responses.calls) == 1
 
 
@@ -80,15 +85,15 @@ def test_broker_authn_show(mocked_cmd, mocked_responses: responses):
     "records",
     [0, 2],
 )
-def test_broker_authn_list(mocked_cmd, mocked_responses: responses, records: int):
+def test_broker_authz_list(mocked_cmd, mocked_responses: responses, records: int):
     broker_name = generate_random_string()
     instance_name = generate_random_string()
     resource_group_name = generate_random_string()
 
-    mock_broker_authn_records = {
+    mock_broker_authz_records = {
         "value": [
-            get_mock_broker_authn_record(
-                authn_name=generate_random_string(),
+            get_mock_broker_authz_record(
+                authz_name=generate_random_string(),
                 broker_name=broker_name,
                 instance_name=instance_name,
                 resource_group_name=resource_group_name,
@@ -99,16 +104,16 @@ def test_broker_authn_list(mocked_cmd, mocked_responses: responses, records: int
 
     mocked_responses.add(
         method=responses.GET,
-        url=get_broker_authn_endpoint(
+        url=get_broker_authz_endpoint(
             broker_name=broker_name, instance_name=instance_name, resource_group_name=resource_group_name
         ),
-        json=mock_broker_authn_records,
+        json=mock_broker_authz_records,
         status=200,
         content_type="application/json",
     )
 
     result = list(
-        list_broker_authns(
+        list_broker_authzs(
             cmd=mocked_cmd,
             mq_broker_name=broker_name,
             instance_name=instance_name,
@@ -116,5 +121,5 @@ def test_broker_authn_list(mocked_cmd, mocked_responses: responses, records: int
         )
     )
 
-    assert result == mock_broker_authn_records["value"]
+    assert result == mock_broker_authz_records["value"]
     assert len(mocked_responses.calls) == 1
