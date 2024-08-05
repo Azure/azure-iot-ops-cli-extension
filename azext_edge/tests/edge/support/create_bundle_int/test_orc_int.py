64c9d20909d02b2210b7fbc9f0b974854a3e2b9a
Elsie4ever <3467996@gmail.com>
2024-08-05 08:06:51 -0700

    feat: exclude evicted pod log from support bundle (#292)

--- azext_edge/tests/edge/support/create_bundle_int/test_orc_int.py
+++ azext_edge/tests/edge/support/create_bundle_int/test_orc_int.py
@@ -7,7 +7,12 @@
 from knack.log import get_logger
 from azext_edge.edge.common import OpsServiceType
 from azext_edge.edge.providers.edge_api import ORC_API_V1
-from .helpers import check_custom_resource_files, check_workload_resource_files, get_file_map, run_bundle_command
+from .helpers import (
+    check_custom_resource_files,
+    check_workload_resource_files,
+    get_file_map,
+    run_bundle_command
+)
 
 logger = get_logger(__name__)
 
@@ -16,7 +21,7 @@ def test_create_bundle_orc(init_setup, tracked_files):
     """Test for ensuring file names and content. ONLY CHECKS orc."""
     ops_service = OpsServiceType.orc.value
     command = f"az iot ops support create-bundle --ops-service {ops_service}"
-    walk_result = run_bundle_command(command=command, tracked_files=tracked_files)
+    walk_result, bundle_path = run_bundle_command(command=command, tracked_files=tracked_files)
     file_map = get_file_map(walk_result, ops_service)["aio"]
 
     check_custom_resource_files(
@@ -28,4 +33,9 @@ def test_create_bundle_orc(init_setup, tracked_files):
     expected_types = set(expected_workload_types).union(ORC_API_V1.kinds)
     assert set(file_map.keys()).issubset(expected_types)
 
-    check_workload_resource_files(file_map, expected_workload_types, ["aio-cert", "aio-orc"])
+    check_workload_resource_files(
+        file_objs=file_map,
+        expected_workload_types=expected_workload_types,
+        prefixes=["aio-cert", "aio-orc"],
+        bundle_path=bundle_path
+    )
