c03632f029cc2482fb86a19174cd1c5a831728a9
Paymaun Heidari <digimaun@users.noreply.github.com>
2024-08-23 18:25:54 -0700

    WIP

--- azext_edge/edge/providers/orchestration/work.py
+++ azext_edge/edge/providers/orchestration/work.py
@@ -3,6 +3,9 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License file in the project root for license information.
 # ----------------------------------------------------------------------------------------------
+# pylint: disable=W0101,E1101
+# TODO - @digimaun
+
 
 from enum import IntEnum
 from json import dumps
