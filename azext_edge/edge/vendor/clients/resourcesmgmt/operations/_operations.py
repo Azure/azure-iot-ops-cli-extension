95a3e8eb6e2ba3a8a58f40fa84cfdfb1bc19d216
Paymaun Heidari <digimaun@users.noreply.github.com>
2024-08-28 16:15:37 -0700

    WIP

--- azext_edge/edge/vendor/clients/resourcesmgmt/operations/_operations.py
+++ azext_edge/edge/vendor/clients/resourcesmgmt/operations/_operations.py
@@ -22810,7 +22810,7 @@ class ResourcesOperations:  # pylint: disable=too-many-public-methods
          None.
         :paramtype filter: str
         :keyword expand: Comma-separated list of additional properties to be included in the response.
-         Valid values include ``createdTime``\ , ``changedTime`` and ``provisioningState``. For example,
+         Valid values include ``createdTime``, ``changedTime`` and ``provisioningState``. For example,
          ``$expand=createdTime,changedTime``. Default value is None.
         :paramtype expand: str
         :keyword top: The number of results to return. If null is passed, returns all resources.
@@ -23381,16 +23381,16 @@ class ResourcesOperations:  # pylint: disable=too-many-public-methods
 
         :keyword filter: The filter to apply on the operation.:code:`<br>`:code:`<br>`Filter comparison
          operators include ``eq`` (equals) and ``ne`` (not equals) and may be used with the following
-         properties: ``location``\ , ``resourceType``\ , ``name``\ , ``resourceGroup``\ , ``identity``\
-         , ``identity/principalId``\ , ``plan``\ , ``plan/publisher``\ , ``plan/product``\ ,
-         ``plan/name``\ , ``plan/version``\ , and ``plan/promotionCode``.:code:`<br>`:code:`<br>`For
+         properties: ``location``, ``resourceType``, ``name``, ``resourceGroup``, ``identity``
+         , ``identity/principalId``, ``plan``, ``plan/publisher``, ``plan/product``,
+         ``plan/name``, ``plan/version``, and ``plan/promotionCode``.:code:`<br>`:code:`<br>`For
          example, to filter by a resource type, use ``$filter=resourceType eq
-         'Microsoft.Network/virtualNetworks'``\ :code:`<br>`:code:`<br>`:code:`<br>`\
+         'Microsoft.Network/virtualNetworks'``:code:`<br>`:code:`<br>`:code:`<br>`
          ``substringof(value, property)`` can  be used to filter for substrings of the following
-         currently-supported properties: ``name`` and ``resourceGroup``\ :code:`<br>`:code:`<br>`For
+         currently-supported properties: ``name`` and ``resourceGroup`` :code:`<br>`:code:`<br>`For
          example, to get all resources with 'demo' anywhere in the resource name, use
-         ``$filter=substringof('demo', name)``\ :code:`<br>`:code:`<br>`Multiple substring operations
-         can also be combined using ``and``\ /\ ``or`` operators.:code:`<br>`:code:`<br>`Note that any
+         ``$filter=substringof('demo', name)`` :code:`<br>`:code:`<br>`Multiple substring operations
+         can also be combined using ``and``/``or`` operators.:code:`<br>`:code:`<br>`Note that any
          truncated number of results queried via ``$top`` may also not be compatible when using a
          filter.:code:`<br>`:code:`<br>`:code:`<br>`Resources can be filtered by tag names and values.
          For example, to filter for a tag name and value, use ``$filter=tagName eq 'tag1' and tagValue
@@ -23399,14 +23399,14 @@ class ResourcesOperations:  # pylint: disable=too-many-public-methods
          additional properties queried via ``$expand`` may also not be compatible when filtering by tag
          names/values. :code:`<br>`:code:`<br>`For tag names only, resources can be filtered by prefix
          using the following syntax: ``$filter=startswith(tagName, 'depart')``. This query will return
-         all resources with a tag name prefixed by the phrase ``depart`` (i.e.\ ``department``\ ,
-         ``departureDate``\ , ``departureTime``\ , etc.):code:`<br>`:code:`<br>`:code:`<br>`Note that
+         all resources with a tag name prefixed by the phrase ``depart`` (i.e.``department``,
+         ``departureDate``, ``departureTime``, etc.):code:`<br>`:code:`<br>`:code:`<br>`Note that
          some properties can be combined when filtering resources, which include the following:
-         ``substringof() and/or resourceType``\ , ``plan and plan/publisher and plan/name``\ , and
+         ``substringof() and/or resourceType``, ``plan and plan/publisher and plan/name``, and
          ``identity and identity/principalId``. Default value is None.
         :paramtype filter: str
         :keyword expand: Comma-separated list of additional properties to be included in the response.
-         Valid values include ``createdTime``\ , ``changedTime`` and ``provisioningState``. For example,
+         Valid values include ``createdTime``, ``changedTime`` and ``provisioningState``. For example,
          ``$expand=createdTime,changedTime``. Default value is None.
         :paramtype expand: str
         :keyword top: The number of recommendations per page if a paged version of this API is being
