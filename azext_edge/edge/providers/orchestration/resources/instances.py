f680d85c3cbc440030d91c374d29764878357af8
Paymaun Heidari <digimaun@users.noreply.github.com>
2024-09-16 16:27:07 -0700

    WIP

--- azext_edge/edge/providers/orchestration/resources/instances.py
+++ azext_edge/edge/providers/orchestration/resources/instances.py
@@ -192,7 +192,7 @@ class Instances(Queryable):
         mi_resource_id_container = parse_resource_id(mi_user_assigned)
         instance = self.show(name=name, resource_group_name=resource_group_name)
         cluster_resource = self.get_resource_map(instance).connected_cluster.resource
-        self._ensure_oidc_issuer(cluster_resource)
+        oidc_issuer_url_key = self._ensure_oidc_issuer(cluster_resource, use_self_hosted_issuer)
         custom_location = self._get_associated_cl(instance)
         if not federated_credential_name:
             federated_credential_name = get_fc_name(
@@ -200,10 +200,9 @@ class Instances(Queryable):
                 namespace=custom_location["properties"]["namespace"],
                 instance_name=instance["name"],
             )
-        issuer_key = "selfHostedIssuerUrl" if use_self_hosted_issuer else "issuerUrl"
         self.federate_msi(
             mi_resource_id_container,
-            oidc_issuer=cluster_resource["properties"]["oidcIssuerProfile"][issuer_key],
+            oidc_issuer=cluster_resource["properties"]["oidcIssuerProfile"][oidc_issuer_url_key],
             federated_credential_name=federated_credential_name,
             namespace=custom_location["properties"]["namespace"],
         )
@@ -250,7 +249,7 @@ class Instances(Queryable):
             cluster_resource = resource_map.connected_cluster.resource
             custom_location = self._get_associated_cl(instance)
             cl_resources = resource_map.connected_cluster.get_aio_resources(custom_location_id=custom_location["id"])
-            self._ensure_oidc_issuer(cluster_resource)
+            oidc_issuer_url_key = self._ensure_oidc_issuer(cluster_resource, use_self_hosted_issuer)
             secretsync_spc = self._find_existing_spc(cl_resources)
             if secretsync_spc:
                 status.stop()
@@ -268,10 +267,9 @@ class Instances(Queryable):
                     )
                     + "-ssc"
                 )
-            issuer_key = "selfHostedIssuerUrl" if use_self_hosted_issuer else "issuerUrl"
             self.federate_msi(
                 mi_resource_id_container=mi_resource_id_container,
-                oidc_issuer=cluster_resource["properties"]["oidcIssuerProfile"][issuer_key],
+                oidc_issuer=cluster_resource["properties"]["oidcIssuerProfile"][oidc_issuer_url_key],
                 federated_credential_name=federated_credential_name,
                 namespace=custom_location["properties"]["namespace"],
                 service_account_name=SERVICE_ACCOUNT_SECRETSYNC,
@@ -345,7 +343,7 @@ class Instances(Queryable):
 
     def _attempt_keyvault_role_assignments(self, keyvault: dict, mi_user_assigned: dict) -> Optional[str]:
         """
-        Returns error string is role-assignment fails.
+        Returns error string if the role-assignment fails.
         """
         target_role_ids = [KEYVAULT_ROLE_ID_SECRETS_USER, KEYVAULT_ROLE_ID_READER]
         try:
@@ -365,7 +363,7 @@ class Instances(Queryable):
                 scope=keyvault["id"],
             )
 
-    def _ensure_oidc_issuer(self, cluster_resource: dict):
+    def _ensure_oidc_issuer(self, cluster_resource: dict, use_self_hosted_issuer: Optional[bool] = None) -> str:
         enabled_oidc = cluster_resource["properties"].get("oidcIssuerProfile", {}).get("enabled", False)
         enabled_wlif = (
             cluster_resource["properties"].get("securityProfile", {}).get("workloadIdentity", {}).get("enabled", False)
@@ -388,6 +386,16 @@ class Instances(Queryable):
 
         if any([not enabled_oidc, not enabled_wlif]):
             raise ValidationError(error)
+
+        oidc_issuer_profile: dict = cluster_resource["properties"]["oidcIssuerProfile"]
+        oidc_issuer_key = "issuerUrl"
+        if use_self_hosted_issuer:
+            oidc_issuer_key = "selfHostedIssuerUrl"
+        oidc_issuer_url = oidc_issuer_profile.get(oidc_issuer_key)
+        if not oidc_issuer_url:
+            raise ValidationError(f"{oidc_issuer_key} is not configured.")
+
+        return oidc_issuer_key
 
     def federate_msi(
         self,
