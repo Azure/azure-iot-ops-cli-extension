7185ca51df5e6b320e87803e0a22a239df9b3766
Paymaun Heidari <digimaun@users.noreply.github.com>
2024-09-27 19:06:18 -0700

    WIP

--- azext_edge/tests/edge/orchestration/test_targets_unit.py
+++ azext_edge/tests/edge/orchestration/test_targets_unit.py
@@ -12,6 +12,7 @@ from azext_edge.edge.providers.orchestration.targets import (
     TRUST_SETTING_KEYS,
     InitTargets,
     assemble_nargs_to_dict,
+    get_insecure_listener,
 )
 
 from ...generators import generate_random_string
@@ -36,13 +37,41 @@ def get_trust_settings():
 K8S_NAME_KEYS = frozenset(["cluster_namespace", "custom_location_name", "instance_name"])
 KEY_CONVERSION_MAP = {"enable_rsync_rules": "deploy_resource_sync_rules"}
 KVP_KEYS = frozenset(["ops_config", "trust_settings"])
-ENABLEMENT_PARAM_CONVERSION_MAP = {"clusterName": "cluster_name", "kubernetesDistro": "kubernetes_distro", "trustConfig": "trust_config", "advancedConfig": "advanced_config"}
+ENABLEMENT_PARAM_CONVERSION_MAP = {
+    "clusterName": "cluster_name",
+    "kubernetesDistro": "kubernetes_distro",
+    "containerRuntimeSocket": "container_runtime_socket",
+    "trustConfig": "trust_config",
+    "schemaRegistryId": "schema_registry_resource_id",
+    "advancedConfig": "advanced_config",
+}
+INSTANCE_PARAM_CONVERSION_MAP = {
+    "clusterName": "cluster_name",
+    "clusterNamespace": "cluster_namespace",
+    "clusterLocation": "location",
+    "customLocationName": "custom_location_name",
+    "deployResourceSyncRules": "deploy_resource_sync_rules",
+    "schemaRegistryId": "schema_registry_resource_id",
+    "defaultDataflowinstanceCount": "dataflow_profile_instances",
+    "brokerConfig": "broker_config",
+}
 
 
 @pytest.mark.parametrize(
     "target_scenario",
     [
         build_target_scenario(cluster_name=generate_random_string(), resource_group_name=generate_random_string()),
+        build_target_scenario(
+            cluster_name=generate_random_string(),
+            resource_group_name=generate_random_string(),
+            schema_registry_resource_id=get_resource_id(
+                resource_path="/schemaRegistries/myregistry",
+                resource_group_name=generate_random_string(),
+                resource_provider="Microsoft.DeviceRegistry",
+            ),
+            location=generate_random_string(),
+            instance_name=generate_random_string(),
+        ),
         build_target_scenario(
             cluster_name=generate_random_string(),
             resource_group_name=generate_random_string(),
@@ -72,7 +101,7 @@ ENABLEMENT_PARAM_CONVERSION_MAP = {"clusterName": "cluster_name", "kubernetesDis
             add_insecure_listener=True,
             kubernetes_distro=generate_random_string(),
             container_runtime_socket=generate_random_string(),
-            # TODO - missing broker config
+            custom_broker_config={generate_random_string(): generate_random_string()},
         ),
     ],
 )
@@ -94,15 +123,6 @@ def test_init_targets(target_scenario: dict):
             target_scenario[scenario_key] == targets_value
         ), f"{scenario_key} input mismatch with equivalent targets {targets_key} value."
 
-
-    enablement_template, enablement_parameters = targets.get_ops_enablement_template()
-    # for parameter in enablement_parameters:
-    #     if parameter in ENABLEMENT_PARAM_CONVERSION_MAP:
-    #         assert target_scenario[ENABLEMENT_PARAM_CONVERSION_MAP[parameter]["value"]] == 
-
-
-    #instance_template, instance_parameters = targets.get_ops_instance_template()
-    import pdb; pdb.set_trace()
     if target_scenario.get("enable_fault_tolerance"):
         assert targets.advanced_config == {"edgeStorageAccelerator": {"faultToleranceEnabled": True}}
 
@@ -116,3 +136,55 @@ def test_init_targets(target_scenario: dict):
                 "configMapName": target_scenario["trust_settings"]["configMapName"],
             },
         }
+
+    enablement_template, enablement_parameters = targets.get_ops_enablement_template()
+    for parameter in enablement_parameters:
+        targets_key = parameter
+        if parameter in ENABLEMENT_PARAM_CONVERSION_MAP:
+            targets_key = ENABLEMENT_PARAM_CONVERSION_MAP[parameter]
+        assert enablement_parameters[parameter]["value"] == getattr(
+            targets, targets_key
+        ), f"{parameter} value mismatch with targets {targets_key} value."
+
+    if targets.ops_config:
+        aio_config_settings = enablement_template["variables"]["defaultAioConfigurationSettings"]
+        for c in targets.ops_config:
+            assert c in aio_config_settings
+            assert aio_config_settings[c] == targets.ops_config[c]
+
+    if targets.ops_version:
+        assert enablement_template["variables"]["VERSIONS"]["aio"] == targets.ops_version
+
+    extension_ids = [generate_random_string(), generate_random_string()]
+    instance_template, instance_parameters = targets.get_ops_instance_template(extension_ids)
+    for parameter in instance_parameters:
+        if parameter == "clExtentionIds":
+            assert instance_parameters[parameter]["value"] == extension_ids
+            continue
+        targets_key = parameter
+        if parameter in INSTANCE_PARAM_CONVERSION_MAP:
+            targets_key = INSTANCE_PARAM_CONVERSION_MAP[parameter]
+        assert instance_parameters[parameter]["value"] == getattr(
+            targets, targets_key
+        ), f"{parameter} value mismatch with targets {targets_key} value."
+
+    assert instance_template["resources"]["aioInstance"]["properties"]["description"] == targets.instance_description
+
+    if targets.tags:
+        assert instance_template["resources"]["aioInstance"]["tags"] == targets.tags
+
+    if targets.instance_name:
+        assert instance_template["resources"]["aioInstance"]["name"] == targets.instance_name
+        assert instance_template["resources"]["broker"]["name"] == f"{targets.instance_name}/default"
+        assert instance_template["resources"]["broker_authn"]["name"] == f"{targets.instance_name}/default/default"
+        assert instance_template["resources"]["broker_listener"]["name"] == f"{targets.instance_name}/default/default"
+        assert instance_template["resources"]["dataflow_profile"]["name"] == f"{targets.instance_name}/default"
+        assert instance_template["resources"]["dataflow_endpoint"]["name"] == f"{targets.instance_name}/default"
+
+    if targets.custom_broker_config:
+        assert instance_template["resources"]["broker"]["properties"] == targets.custom_broker_config
+
+    if targets.add_insecure_listener:
+        assert instance_template["resources"]["broker_listener_insecure"] == get_insecure_listener(
+            targets.instance_name, "default"
+        )
