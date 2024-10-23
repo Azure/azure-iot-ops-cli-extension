204bb49997ebb12bdbf56a745e5bfe0d34336818
Ryan K <ryan.k@outlook.com>
2024-10-22 17:11:36 -0700

    refactor: update int tests to move init checks to create (#418)

--- azext_edge/tests/edge/init/int/test_init_int.py
+++ azext_edge/tests/edge/init/int/test_init_int.py
@@ -44,17 +44,16 @@ def init_test_setup(settings, tracked_resources):
         "--enable-hierarchical-namespace --public-network-access Disabled "
         "--allow-shared-key-access false --allow-blob-public-access false --default-action Deny"
     )
-    tracked_resources.append(storage_account['id'])
+    tracked_resources.append(storage_account["id"])
     registry = run(
         f"az iot ops schema registry create -n {registry_name} -g {settings.env.azext_edge_rg} "
-        f"--rn {registry_namespace} --sa-resource-id {storage_account['id']} "
+        f"--rn {registry_namespace} --sa-resource-id {storage_account['id']}"
     )
     tracked_resources.append(registry["id"])
 
     if not all([settings.env.azext_edge_cluster, settings.env.azext_edge_rg]):
         raise AssertionError(
-            "Cannot run init tests without a connected cluster and resource group. "
-            f"Current settings:\n {settings}"
+            f"Cannot run init tests without a connected cluster and resource group. Current settings:\n {settings}"
         )
 
     yield {
@@ -65,7 +64,7 @@ def init_test_setup(settings, tracked_resources):
         "additionalCreateArgs": _strip_quotes(settings.env.azext_edge_create_args),
         "additionalInitArgs": _strip_quotes(settings.env.azext_edge_init_args),
         "continueOnError": settings.env.azext_edge_init_continue_on_error or False,
-        "redeployment": settings.env.azext_edge_init_redeployment or False
+        "redeployment": settings.env.azext_edge_init_redeployment or False,
     }
     if settings.env.azext_edge_aio_cleanup:
         run(
@@ -74,14 +73,12 @@ def init_test_setup(settings, tracked_resources):
         )
     else:
         # if the init + create worked - make sure that schema reg + storage account aren't deleted
-        tracked_resources.remove(storage_account['id'])
-        tracked_resources.remove(registry['id'])
+        tracked_resources.remove(storage_account["id"])
+        tracked_resources.remove(registry["id"])
 
 
 @pytest.mark.init_scenario_test
-def test_init_scenario(
-    init_test_setup, tracked_files
-):
+def test_init_scenario(init_test_setup, tracked_files):
     additional_init_args = init_test_setup["additionalInitArgs"] or ""
     init_arg_dict = _process_additional_args(additional_init_args)
     additional_create_args = init_test_setup["additionalCreateArgs"] or ""
@@ -92,8 +89,7 @@ def test_init_scenario(
     resource_group = init_test_setup["resourceGroup"]
     registry_id = init_test_setup["schemaRegistryId"]
     instance_name = init_test_setup["instanceName"]
-    command = f"az iot ops init -g {resource_group} --cluster {cluster_name} "\
-        f"--no-progress {additional_init_args} "
+    command = f"az iot ops init -g {resource_group} --cluster {cluster_name} --no-progress {additional_init_args} "
 
     # TODO: assert return once there is a return for init
     run(command)
@@ -101,17 +97,16 @@ def test_init_scenario(
     assert_aio_init(cluster_name=cluster_name, resource_group=resource_group, **init_arg_dict)
 
     # create command
-    create_command = f"az iot ops create -g {resource_group} --cluster {cluster_name} "\
-        f"--sr-resource-id {registry_id}  -n {instance_name} "\
+    create_command = (
+        f"az iot ops create -g {resource_group} --cluster {cluster_name} "
+        f"--sr-resource-id {registry_id}  -n {instance_name} "
         f"--no-progress {additional_create_args} "
+    )
     # TODO: assert create when return be returning
     run(create_command)
 
     if init_test_setup["redeployment"]:
-        run(
-            f"az iot ops delete --name {instance_name} -g {resource_group} "
-            "-y --no-progress --force"
-        )
+        run(f"az iot ops delete --name {instance_name} -g {resource_group} -y --no-progress --force")
         run(create_command)
 
     # Missing:
@@ -133,7 +128,7 @@ def test_init_scenario(
                 cluster_name=cluster_name,
                 resource_group=resource_group,
                 schema_registry_id=registry_id,
-                **create_arg_dict
+                **create_arg_dict,
             )
     except Exception as e:  # pylint: disable=broad-except
         # Note we have this since there are multiple Exceptions that can occur:
@@ -150,7 +145,6 @@ def test_init_scenario(
 def assert_aio_init(
     cluster_name: str,
     resource_group: str,
-    ops_config: Optional[str] = None,
     **_,
 ):
     # check extensions installed
@@ -166,21 +160,12 @@ def assert_aio_init(
     while extension_result.get("nextLink"):
         extension_result = run(f"az rest --method GET --url {extension_result['nextLink']}")
         extensions.extend(extension_result["value"])
-    iot_ops_ext = None,
     iot_ops_platform_ext = None
     for ext in extensions:
-        if ext["properties"]["extensionType"] == "microsoft.iotoperations":
-            iot_ops_ext = ext
-        elif ext["properties"]["extensionType"] == "microsoft.iotoperations.platform":
+        if ext["properties"]["extensionType"] == "microsoft.iotoperations.platform":
             iot_ops_platform_ext = ext
 
-    if ops_config:
-        ops_config = assemble_nargs_to_dict(ops_config.split())
-        configs = iot_ops_ext["properties"]["configurationSettings"]
-        for key, value in ops_config.items():
-            assert configs[key] == value
-
-    if not all([iot_ops_platform_ext, iot_ops_ext]):
+    if not all([iot_ops_platform_ext]):
         raise AssertionError(
             "Extensions for AIO are missing. These are the extensions "
             f"on the cluster: {[ext['name'] for ext in extensions]}."
@@ -189,15 +174,47 @@ def assert_aio_init(
 
 def assert_aio_instance(
     instance_name: str,
+    cluster_name: str,
     resource_group: str,
     schema_registry_id: str,
+    ops_config: Optional[str] = None,
     custom_location: Optional[str] = None,
     description: Optional[str] = None,
     location: Optional[str] = None,
     enable_rsync: bool = False,
     tags: Optional[str] = None,
-    **_
+    **_,
 ):
+    # check extensions installed
+    cluster_id = run(
+        f"az resource show -n {cluster_name} -g {resource_group} "
+        "--resource-type Microsoft.Kubernetes/connectedClusters"
+    )["id"]
+    extension_result = run(
+        f"az rest --method GET --url {cluster_id}/providers/"
+        "Microsoft.KubernetesConfiguration/extensions?api-version=2023-05-01"
+    )
+    extensions = extension_result["value"]
+    while extension_result.get("nextLink"):
+        extension_result = run(f"az rest --method GET --url {extension_result['nextLink']}")
+        extensions.extend(extension_result["value"])
+    iot_ops_ext = None
+    for ext in extensions:
+        if ext["properties"]["extensionType"] == "microsoft.iotoperations":
+            iot_ops_ext = ext
+
+    if ops_config:
+        ops_config = assemble_nargs_to_dict(ops_config.split())
+        configs = iot_ops_ext["properties"]["configurationSettings"]
+        for key, value in ops_config.items():
+            assert configs[key] == value
+
+    if not all([iot_ops_ext]):
+        raise AssertionError(
+            "Extensions for AIO are missing. These are the extensions "
+            f"on the cluster: {[ext['name'] for ext in extensions]}."
+        )
+
     instance_show = run(f"az iot ops show -n {instance_name} -g {resource_group}")
     tags = assemble_nargs_to_dict(tags)
     assert instance_show.get("tags", {}) == tags
@@ -210,27 +227,6 @@ def assert_aio_instance(
     instance_props = instance_show["properties"]
     assert instance_props.get("description") == description
     assert instance_props["schemaRegistryRef"] == {"resource_id": schema_registry_id}
-
-    expected_components = {"adr", "akri", "connectors", "dataflows", "schemaRegistry"}
-    disabled_components = []
-    unexpected_components = []
-    for component, state in instance_props["components"].items():
-        if state["state"].lower() != "enabled":
-            disabled_components.append(component)
-        if component in expected_components:
-            expected_components.remove(component)
-        else:
-            unexpected_components.append(component)
-
-    error_msg = []
-    if disabled_components:
-        error_msg.append(f"The following components are disabled: {disabled_components}.")
-    if unexpected_components:
-        error_msg.append(f"The following components are unexpected: {unexpected_components}.")
-    if expected_components:
-        error_msg.append(f"The following components are missing: {expected_components}.")
-    if error_msg:
-        raise AssertionError("\n".join(error_msg))
 
     tree = run(f"az iot ops show -n {instance_name} -g {resource_group} --tree")
     # no resource sync rules if disable rsync rules
@@ -258,7 +254,7 @@ def assert_broker_args(
     fw: Optional[str] = None,
     lt: Optional[str] = None,
     mp: Optional[str] = None,
-    **_
+    **_,
 ):
     if bp:
         broker_backend_part = bp
@@ -317,7 +313,7 @@ def assert_broker_args(
     if add_insecure_listener:
         insecure = [listener for listener in listeners if listener["name"] == "default-insecure"][0]
         ports = insecure["properties"]["ports"]
-        assert 1883 in [p['port'] for p in ports]
+        assert 1883 in [p["port"] for p in ports]
 
     secure_listener = [listener for listener in listeners if listener["name"] == "default"]
     listener_props = secure_listener[0]["properties"]
@@ -325,10 +321,7 @@ def assert_broker_args(
 
 
 def assert_dataflow_profile_args(
-    instance_name: str,
-    resource_group: str,
-    dataflow_profile_instances: Optional[int] = None,
-    **_
+    instance_name: str, resource_group: str, dataflow_profile_instances: Optional[int] = None, **_
 ):
     profile = run(f"az iot ops dataflow profile list -g {resource_group} -i {instance_name}")
     profile_props = profile[0]["properties"]
@@ -368,44 +361,22 @@ def _strip_quotes(argument: Optional[str]) -> Optional[str]:
 
 
 DEFAULT_BROKER_CONFIG = {
-    "advanced": {
-        "encryptInternalTraffic": "Enabled"
-    },
+    "advanced": {"encryptInternalTraffic": "Enabled"},
     "cardinality": {
-        "backendChain": {
-            "partitions": 2,
-            "redundancyFactor": 2,
-            "workers": 2
-        },
-        "frontend": {
-            "replicas": 2,
-            "workers": 2
-        }
+        "backendChain": {"partitions": 2, "redundancyFactor": 2, "workers": 2},
+        "frontend": {"replicas": 2, "workers": 2},
     },
     "diagnostics": {
-        "logs": {
-            "level": "info"
-        },
-        "metrics": {
-            "prometheusPort": 9600
-        },
-        "selfCheck": {
-            "intervalSeconds": 30,
-            "mode": "Enabled",
-            "timeoutSeconds": 15
-        },
+        "logs": {"level": "info"},
+        "metrics": {"prometheusPort": 9600},
+        "selfCheck": {"intervalSeconds": 30, "mode": "Enabled", "timeoutSeconds": 15},
         "traces": {
             "cacheSizeMegabytes": 16,
             "mode": "Enabled",
-            "selfTracing": {
-                "intervalSeconds": 30,
-                "mode": "Enabled"
-            },
-            "spanChannelCapacity": 1000
-        }
+            "selfTracing": {"intervalSeconds": 30, "mode": "Enabled"},
+            "spanChannelCapacity": 1000,
+        },
     },
-    "generateResourceLimits": {
-        "cpu": "Disabled"
-    },
+    "generateResourceLimits": {"cpu": "Disabled"},
     "memoryProfile": "Medium",
 }
