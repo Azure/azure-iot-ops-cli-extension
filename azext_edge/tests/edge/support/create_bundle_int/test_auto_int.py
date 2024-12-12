a0f901c23cdd899959b20aebefe57ca54ee66eec
Elsie Ju <jiacju@microsoft.com>
2024-12-11 17:47:51 -0800

    int test6

--- azext_edge/tests/edge/support/create_bundle_int/test_auto_int.py
+++ azext_edge/tests/edge/support/create_bundle_int/test_auto_int.py
@@ -83,17 +83,14 @@ def test_create_bundle(init_setup, bundle_dir, mq_traces, ops_service, tracked_f
             ]
             walk_result[path.join(BASE_ZIP_PATH, aio_namespace, OpsServiceType.mq.value)]["folders"] = []
 
-    # remove acs resources from walk_result from aio namespace assertion
-    if acs_namespace:
-        walk_result.pop(path.join(BASE_ZIP_PATH, acs_namespace, "arccontainerstorage"), {})
-
-    # remove acstor resources from walk_result from aio namespace assertion
-    if acstor_namespace:
-        walk_result.pop(path.join(BASE_ZIP_PATH, acstor_namespace, "containerstorage"), {})
-
-    # remove ssc resources in ssc namespace from walk_result from aio namespace assertion
-    if ssc_namespace:
-        walk_result.pop(path.join(BASE_ZIP_PATH, ssc_namespace, OpsServiceType.secretstore.value), {})
+    # remove other namespaces resource from aio namespace assertion
+    for namespace, service in [
+        (acs_namespace, "arccontainerstorage"),
+        (acstor_namespace, "containerstorage"),
+        (ssc_namespace, OpsServiceType.secretstore.value),
+    ]:
+        if namespace:
+            walk_result.pop(path.join(BASE_ZIP_PATH, namespace, service), {})
 
     # remove azuremonitor resources in arc namespace from walk_result from aio namespace assertion
     if arc_namespace and path.join(BASE_ZIP_PATH, arc_namespace, OpsServiceType.azuremonitor.value) in walk_result:
