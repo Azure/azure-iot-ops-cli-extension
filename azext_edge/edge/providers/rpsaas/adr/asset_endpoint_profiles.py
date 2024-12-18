c5978bb11a7853ddd4fe44c510465bcb9583574c
Victoria Litvinova <vilit@microsoft.com>
2024-12-18 13:33:43 -0800

    bumming

--- azext_edge/edge/providers/rpsaas/adr/asset_endpoint_profiles.py
+++ azext_edge/edge/providers/rpsaas/adr/asset_endpoint_profiles.py
@@ -218,20 +218,6 @@ class AssetEndpointProfiles(Queryable):
 
 
 # Helpers
-def _assert_above_min(param: str, value: int, minimum: int = 0) -> str:
-    if value < minimum:
-        return f"The parameter {param} needs to be at least {minimum}.\n"
-    return ""
-
-
-def _raise_if_connector_error(connector_type: str, error_msg: str):
-    if error_msg:
-        raise InvalidArgumentValueError(
-            f"The following {connector_type} connector arguments are invalid:\n {error_msg}"
-        )
-
-
-# TODO: use jsonschema lib
 def _build_opcua_config(
     original_config: Optional[str] = None,
     application_name: Optional[str] = None,
@@ -251,13 +237,14 @@ def _build_opcua_config(
     sub_life_time: Optional[int] = None,
     **_
 ) -> str:
+    from jsonschema import Draft7Validator, ValidationError, validate
+    from .additional_configuration_schemas import OPCUA_SCHEMA
     config = json.loads(original_config) if original_config else {}
+    validator = Draft7Validator(schema=OPCUA_SCHEMA)
 
-    error_msg = ""
     if application_name:
         config["applicationName"] = application_name
     if keep_alive:
-        error_msg += _assert_above_min("--keep-alive", keep_alive)
         config["keepAliveMilliseconds"] = keep_alive
     if run_asset_discovery is not None:
         config["runAssetDiscovery"] = run_asset_discovery
@@ -268,13 +255,10 @@ def _build_opcua_config(
     ]) and not config.get("defaults"):
         config["defaults"] = {}
     if default_publishing_interval:
-        error_msg += _assert_above_min("--default-publishing-int", default_publishing_interval, -1)
         config["defaults"]["publishingIntervalMilliseconds"] = default_publishing_interval
     if default_sampling_interval:
-        error_msg += _assert_above_min("--default-sampling-int", default_sampling_interval, -1)
         config["defaults"]["samplingIntervalMilliseconds"] = default_sampling_interval
     if default_queue_size:
-        error_msg += _assert_above_min("--default-queue-size", default_queue_size, 0)
         config["defaults"]["queueSize"] = default_queue_size
 
     # session
@@ -283,27 +267,21 @@ def _build_opcua_config(
     ]) and not config.get("session"):
         config["session"] = {}
     if session_timeout:
-        error_msg += _assert_above_min("--session-timeout", session_timeout)
         config["session"]["timeoutMilliseconds"] = session_timeout
     if session_keep_alive:
-        error_msg += _assert_above_min("--session-keep-alive", session_keep_alive)
         config["session"]["keepAliveIntervalMilliseconds"] = session_keep_alive
     if session_reconnect_period:
-        error_msg += _assert_above_min("--session-reconnect-period", session_reconnect_period)
         config["session"]["reconnectPeriodMilliseconds"] = session_reconnect_period
     if session_reconnect_exponential_back_off:
-        error_msg += _assert_above_min("--session-reconnect-backoff", session_reconnect_exponential_back_off, -1)
         config["session"]["reconnectExponentialBackOffMilliseconds"] = session_reconnect_exponential_back_off
 
     # subscription
     if any([sub_life_time, sub_max_items]) and not config.get("subscription"):
         config["subscription"] = {}
     if sub_life_time:
-        error_msg += _assert_above_min("--subscription-life-time", sub_life_time)
-        config["subscription"]["maxItems"] = sub_life_time
+        config["subscription"]["lifeTimeMilliseconds"] = sub_life_time
     if sub_max_items:
-        error_msg += _assert_above_min("--subscription-max-items", sub_max_items, 1)
-        config["subscription"]["lifeTimeMilliseconds"] = sub_max_items
+        config["subscription"]["maxItems"] = sub_max_items
 
     # security
     if any([
@@ -317,7 +295,18 @@ def _build_opcua_config(
     if security_policy:
         config["security"]["securityPolicy"] = "http://opcfoundation.org/UA/SecurityPolicy#" + security_policy
 
-    _raise_if_connector_error(connector_type="OPCUA", error_msg=error_msg)
+    # errors = [str(error) for error in validator.iter_errors(config)]
+    errors = []
+    error: ValidationError
+    for error in validator.iter_errors(config):
+        errors.append(f"- argument {error.schema['description']}: {error.message}")
+
+    if errors:
+        raise InvalidArgumentValueError(
+            "Invalid Additional Configuration arguments:\n" +
+            "\n".join(errors)
+        )
+
     return json.dumps(config)
 
 
