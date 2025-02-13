07b75261c1482dfa8da74b86e2d351078712c32a
Paymaun <digimaun@users.noreply.github.com>
2025-02-13 12:17:13 -0800

    feat!: block downgrade (#493)
    * Hidden flag `--force` is added to override the block if necessary.

--- azext_edge/constants.py
+++ azext_edge/constants.py
@@ -7,7 +7,7 @@
 
 import os
 
-VERSION = "1.2.0b2"
+VERSION = "1.2.0b3"
 EXTENSION_NAME = "azure-iot-ops"
 EXTENSION_ROOT = os.path.dirname(os.path.abspath(__file__))
 USER_AGENT = "IotOperationsCliExtension/{}".format(VERSION)
