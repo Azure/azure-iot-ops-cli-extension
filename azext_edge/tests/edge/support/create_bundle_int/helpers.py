50a2e55b7175116ec0790fafe6e94fe614e72358
Elsie Ju <jiacju@microsoft.com>
2025-03-25 10:55:32 -0700

    update

--- azext_edge/tests/edge/support/create_bundle_int/helpers.py
+++ azext_edge/tests/edge/support/create_bundle_int/helpers.py
@@ -602,7 +602,8 @@ def _clean_up_folders(
             if namespace_folder == arc_namespace and monitor_path in walk_result:
                 monikers.append(OpsServiceType.azuremonitor.value)
             assert set(level_1["folders"]) == set(monikers), (
-                f"Mismatch; folders: [{level_1['folders']}], " f"monikers: [{monikers}]"
+                f"Mismatch; folders: [{level_1['folders']}],"\
+                f"monikers: [{monikers}]"
             )
             assert not level_1["files"]
 
@@ -614,11 +615,13 @@ def _clean_up_folders(
 
         if certmanager_namespace:
             assert set(level_1["folders"]) == set(services), (
-                f"Mismatch; folders: [{level_1['folders']}], " f"monikers: [{services}]"
+                f"Mismatch; folders: [{level_1['folders']}], "\
+                f"monikers: [{services}]"
             )
         else:
             assert level_1["folders"] == [OpsServiceType.secretstore.value], (
-                f"Mismatch; folders: [{level_1['folders']}], " f"monikers: [{OpsServiceType.secretstore.value}]"
+                f"Mismatch; folders: [{level_1['folders']}], "\
+                f"monikers: [{OpsServiceType.secretstore.value}]"
             )
 
     # note that the acstor and acs namespace should be the same value
@@ -637,7 +640,8 @@ def _clean_up_folders(
         ):
             services.append(containerstorage_service)
         assert set(level_1["folders"]) == set(services), (
-            f"Mismatch; folders: [{level_1['folders']}], " f"services [{services}]"
+            f"Mismatch; folders: [{level_1['folders']}], "\
+            f"services [{services}]"
         )
         assert not level_1["files"]
 
