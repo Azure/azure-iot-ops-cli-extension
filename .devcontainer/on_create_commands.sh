0e765e897321dabdaebb10a554f70e09d9c224b9
Ryan K <ryan.k@outlook.com>
2025-03-26 14:36:48 -0700

    feat: devcontainer updates
    Consolidating explore-iot-operations tooling with our devcontainer setup

---
+++ .devcontainer/on_create_commands.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+echo "Starting On Create Command"
+
+# Copy the custom first run notice over
+sudo cp .devcontainer/welcome.txt /usr/local/etc/vscode-dev-containers/first-run-notice.txt
+
+# wait for docker
+until docker version > /dev/null 2>&1
+do
+  echo "Checking if docker daemon has started..."
+  sleep 10s
+done
+
+# create local k3s cluster
+echo "Creating k3d cluster"
+k3d cluster delete
+k3d cluster create \
+-p '1883:1883@loadbalancer' \
+-p '8883:8883@loadbalancer'
--- .devcontainer/post_start_commands.sh
+++ .devcontainer/post_start_commands.sh
@@ -1,46 +1,32 @@
-# wait for docker
-until docker version > /dev/null 2>&1
-do
-  echo "Checking if docker daemon has started..."
-  sleep 10s
-done
-
-# create local k3s cluster
-echo "Creating k3d cluster"
-k3d cluster delete
-k3d cluster create \
--p '1883:1883@loadbalancer' \
--p '8883:8883@loadbalancer'
+# CLUSTER NAME
+echo 'export CLUSTER_NAME=${CODESPACE_NAME}' >> ~/.bashrc
+echo 'export KUBECONFIG=~/.kube/config' >> ~/.bashrc
+source ~/.bashrc
 
 # write kubeconfig
 k3d kubeconfig get k3s-default > ~/.kube/config
 
-# install connectedk8s extension
-az extension add -n connectedk8s -y
-
-# local extension install
 echo "Setting up CLI dev environment"
+
+# Install virtualenv
 python -m venv env
 source env/bin/activate
 
+# Install azdev
 echo "Install AZDEV"
 pip install azdev
 
-echo "Install AZ CLI EDGE"
-azdev setup -c EDGE
+# Install CLI core (EDGE) and configure extension repo
+echo "azdev setup and extension config..."
+azdev setup -c EDGE -r ./
 
-echo "Installing local dev extension"
 # install dev requirements (overrides setuptools)
 pip install -r dev_requirements.txt
-pip install -U --target ~/.azure/cliextensions/azure-iot-ops .
 
 # setup tox environment dependencies in parallel, but don't run tests
-echo "Creating local tox environments"
+echo "Creating local tox environments..."
 python -m pip install tox
 tox -np -e lint,python,coverage
-
-echo "Install complete, please activate your environment with 'source env/bin/activate'"
-echo "This should automatically occur the next time you connect to the codespace"
 
 # Run the following to connect your cluster to ARC
 # RESOURCE_GROUP=[your_cluster_resource_group]
