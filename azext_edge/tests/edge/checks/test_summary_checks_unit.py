d2ac2c1dcf003d5df4ae100e6aab25c64d0c8cef
Elsie Ju <jiacju@microsoft.com>
2025-03-27 23:36:26 -0700

    fix

--- azext_edge/tests/edge/checks/test_summary_checks_unit.py
+++ azext_edge/tests/edge/checks/test_summary_checks_unit.py
@@ -275,7 +275,7 @@ def test_summary_checks(
         ("Akri", akri_status),
         (MQ_ACTIVE_API.as_str(), broker_status),
         (DEVICEREGISTRY_API_V1.as_str(), deviceregistry_status),
-        ("Opcua", opcua_status),
+        ("OPCUA", opcua_status),
         (DATAFLOW_API_V1.as_str(), dataflow_status),
     ]:
         assert service in result["postDeployment"][0]["targets"]
