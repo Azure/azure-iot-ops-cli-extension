cc7a78c12b550b6c030cbadde759e4aa88a3f830
Elsie Ju <jiacju@microsoft.com>
2025-04-01 18:48:07 -0700

    update cli extension version

--- azext_edge/constants.py
+++ azext_edge/constants.py
@@ -7,7 +7,7 @@
 
 import os
 
-VERSION = "1.4.0a2"
+VERSION = "1.4.0a3"
 EXTENSION_NAME = "azure-iot-ops"
 EXTENSION_ROOT = os.path.dirname(os.path.abspath(__file__))
 USER_AGENT = "IotOperationsCliExtension/{}".format(VERSION)
