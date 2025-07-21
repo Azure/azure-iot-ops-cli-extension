3fc38b4042628974a706a9beb091a1c642cfc2b4
Victoria Litvinova <vilit@microsoft.com>
2025-07-21 09:27:43 -0700

    omfg forgot to save a file

--- azext_edge/tests/edge/adr/test_namespace_asset_datasets_int.py
+++ azext_edge/tests/edge/adr/test_namespace_asset_datasets_int.py
@@ -12,8 +12,7 @@ from ...helpers import run
 from .namespace_helpers import create_config_file, assert_point_properties, assert_dataset_properties
 
 
-# TODO fix up tests to work with linux
-pytestmark = pytest.mark.rpsaas
+pytestmark = pytest.mark.rpsaas_long_running
 
 
 def test_namespace_custom_asset_dataset_lifecycle_operations(
