5a7232d2edb11df867732c64d2026444935223f2
Victoria Litvinova <vilit@microsoft.com>
2025-07-28 17:16:41 -0700

    loooooooooooooooooooooooooooooooooooooong

--- azext_edge/tests/edge/adr/test_namespace_asset_mgmt_int.py
+++ azext_edge/tests/edge/adr/test_namespace_asset_mgmt_int.py
@@ -14,7 +14,7 @@ from .namespace_helpers import (
 )
 
 
-pytestmark = pytest.mark.rpsaas_long_running
+pytestmark = pytest.mark.long_running
 
 
 def test_namespace_custom_asset_management_group_lifecycle_operations(
