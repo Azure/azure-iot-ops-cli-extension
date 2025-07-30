792649fd263783e12247ce319eade1780050d26d
Paymaun Heidari <digimaun@users.noreply.github.com>
2025-07-30 13:41:15 -0700

    bump CLI version.

--- azext_edge/constants.py
+++ azext_edge/constants.py
@@ -7,7 +7,7 @@
 
 import os
 
-VERSION = "2.0.0b1"
+VERSION = "2.0.0b2"
 EXTENSION_NAME = "azure-iot-ops"
 EXTENSION_ROOT = os.path.dirname(os.path.abspath(__file__))
 USER_AGENT = "IotOperationsCliExtension/{}".format(VERSION)
