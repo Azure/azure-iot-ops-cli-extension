7bd8a73a28e16fccd6bdec7d4a88b7a2bbdfea65
Ketki <ketkinaik@microsoft.com>
2025-09-29 09:57:57 -0700

    Add tests

--- azext_edge/tests/edge/adr/test_namespace_devices_unit.py
+++ azext_edge/tests/edge/adr/test_namespace_devices_unit.py
@@ -1121,9 +1121,10 @@ def test_add_inbound_custom_device_endpoint(
 
 
 @pytest.mark.parametrize("response_status", [200, 400])
-@pytest.mark.parametrize("username_ref, password_ref", [
-    (None, None),              # Anonymous auth
-    ("secretRef:username", "secretRef:password"),  # Username/Password auth
+@pytest.mark.parametrize("cert_ref, username_ref, password_ref", [
+    (None, None, None),              # Anonymous auth
+    (None, "secretRef:username", "secretRef:password"),  # Username/Password auth
+    ("secretRef:certificate", None, None),  # Certificate auth
 ])
 @pytest.mark.parametrize("endpoint_version", [None, "1.0"])
 @pytest.mark.parametrize("endpoints_present, replace", [
@@ -1134,6 +1135,7 @@ def test_add_inbound_custom_device_endpoint(
 def test_add_inbound_media_device_endpoint(
     mocked_cmd,
     mocked_responses: responses,
+    cert_ref: Optional[str],
     username_ref: Optional[str],
     password_ref: Optional[str],
     response_status: int,
@@ -1175,7 +1177,14 @@ def test_add_inbound_media_device_endpoint(
     }
 
     # Set up authentication structure based on auth type
-    if username_ref and password_ref:
+    if cert_ref:
+        expected_endpoint["authentication"] = {
+            "method": ADRAuthModes.certificate.value,
+            "x509Credentials": {
+                "certificateSecretName": cert_ref
+            }
+        }
+    elif username_ref and password_ref:
         expected_endpoint["authentication"] = {
             "method": ADRAuthModes.userpass.value,
             "usernamePasswordCredentials": {
@@ -1243,6 +1252,7 @@ def test_add_inbound_media_device_endpoint(
                 instance_resource_group=instance_resource_group,
                 endpoint_name=endpoint_name,
                 endpoint_address=endpoint_address,
+                certificate_reference=cert_ref,
                 username_reference=username_ref,
                 password_reference=password_ref,
                 endpoint_version=endpoint_version,
@@ -1259,6 +1269,7 @@ def test_add_inbound_media_device_endpoint(
         instance_resource_group=instance_resource_group,
         endpoint_name=endpoint_name,
         endpoint_address=endpoint_address,
+        certificate_reference=cert_ref,
         username_reference=username_ref,
         password_reference=password_ref,
         endpoint_version=endpoint_version,
