ebe40dfedd77b699c438a11613c31d72ed8e8139
Paymaun Heidari <digimaun@users.noreply.github.com>
2025-10-03 17:11:54 -0700

    WIP

--- setup.py
+++ setup.py
@@ -8,71 +8,23 @@
 import re
 import os.path
 from io import open
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
 EXTENSION_REF_NAME = "azext_edge"
 
-# Version extraction inspired from 'requests'
+# Extraction inspired from 'requests'
 with open(os.path.join(EXTENSION_REF_NAME, "constants.py"), "r", encoding="utf-8") as fd:
     constants_raw = fd.read()
-    VERSION = re.search(r'^VERSION\s*=\s*[\'"]([^\'"]*)[\'"]', constants_raw, re.MULTILINE).group(1)
-    PACKAGE_NAME = re.search(r'^EXTENSION_NAME\s*=\s*[\'"]([^\'"]*)[\'"]', constants_raw, re.MULTILINE).group(1)
+    IOTOPS_TARGET = re.search(r'^IOTOPS_TARGET\s*=\s*[\'"]([^\'"]*)[\'"]', constants_raw, re.MULTILINE).group(1)
 
+if not IOTOPS_TARGET:
+    raise RuntimeError("Cannot find AIO release information.")
 
-if not VERSION:
-    raise RuntimeError("Cannot find version information")
-
-if not PACKAGE_NAME:
-    raise RuntimeError("Cannot find package information")
-
-
-DEPENDENCIES = [
-    "rich>=13.6,<14.0",
-    "kubernetes>=29.0,<32.0",
-    "azure-identity>=1.14.1,<1.18.0",
-    "protobuf~=4.25.0",
-    "opentelemetry-proto~=1.20.0",
-    "packaging>=23.2",
-    "semver>=3.0.4,<4",
-]
-
-CLASSIFIERS = [
-    "Development Status :: 5 - Production/Stable",
-    "Intended Audience :: Developers",
-    "Intended Audience :: System Administrators",
-    "Programming Language :: Python",
-    "Programming Language :: Python :: 3 :: Only",
-    "Programming Language :: Python :: 3",
-    "Programming Language :: Python :: 3.9",
-    "Programming Language :: Python :: 3.10",
-    "Programming Language :: Python :: 3.11",
-    "Programming Language :: Python :: 3.12",
-    "Programming Language :: Python :: 3.13",
-]
-
-short_description = "The Azure IoT Operations extension for Azure CLI."
 
 setup(
-    name=PACKAGE_NAME,
-    version=VERSION,
-    python_requires=">=3.9",
-    description=short_description,
-    long_description="{} Intended for power users and/or automation of Azure IoT Operations solutions at scale.".format(
-        short_description
-    ),
-    license="MIT",
-    author="Microsoft",
-    author_email="iotupx@microsoft.com",  # +@digimaun
-    url="https://github.com/azure/azure-iot-ops-cli-extension",
-    classifiers=CLASSIFIERS,
-    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "scripts"]),
-    package_data={
-        EXTENSION_REF_NAME: [
-            "azext_metadata.json",
-        ]
-    },
-    install_requires=DEPENDENCIES,
-    extras_require=None,
-    zip_safe=False,
+    project_urls={
+        "Homepage": "https://github.com/azure/azure-iot-ops-cli-extension",
+        "X-AIO-Target": f"aio://release/{IOTOPS_TARGET}",
+    }
 )
