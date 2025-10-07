db9f7a965b21b73f8926e5240a3d8d100ab57c10
Paymaun Heidari <digimaun@users.noreply.github.com>
2025-10-06 17:27:32 -0700

    WIP

--- azext_edge/edge/providers/orchestration/template.py
+++ azext_edge/edge/providers/orchestration/template.py
@@ -1242,7 +1242,7 @@ TEMPLATE_BLUEPRINT_INSTANCE = TemplateBlueprint(
         },
         "variables": {
             "VERSIONS": {"iotOperations": "1.2.90"},
-            "TRAINS": {"iotOperations": "integration"},
+            "TRAINS": {"iotOperations": "dev"},
             "HASH": "[coalesce(tryGet(parameters('advancedConfig'), 'resourceSuffix'), take(uniqueString(resourceGroup().id, parameters('clusterName'), parameters('clusterNamespace')), 5))]",
             "AIO_EXTENSION_SUFFIX": "[take(uniqueString(resourceId('Microsoft.Kubernetes/connectedClusters', parameters('clusterName'))), 5)]",
             "CUSTOM_LOCATION_NAMESPACE": "[parameters('clusterNamespace')]",
