fed695063157b5b9957ee3c3bae3472231c21604
Paymaun Heidari <digimaun@users.noreply.github.com>
2025-10-07 07:07:03 -0700

    increment

--- azext_edge/edge/providers/orchestration/template.py
+++ azext_edge/edge/providers/orchestration/template.py
@@ -1241,8 +1241,8 @@ TEMPLATE_BLUEPRINT_INSTANCE = TemplateBlueprint(
             "advancedConfig": {"$ref": "#/definitions/_1.AdvancedConfig", "defaultValue": {}},
         },
         "variables": {
-            "VERSIONS": {"iotOperations": "1.2.90"},
-            "TRAINS": {"iotOperations": "dev"},
+            "VERSIONS": {"iotOperations": "1.2.91"},
+            "TRAINS": {"iotOperations": "integration"},
             "HASH": "[coalesce(tryGet(parameters('advancedConfig'), 'resourceSuffix'), take(uniqueString(resourceGroup().id, parameters('clusterName'), parameters('clusterNamespace')), 5))]",
             "AIO_EXTENSION_SUFFIX": "[take(uniqueString(resourceId('Microsoft.Kubernetes/connectedClusters', parameters('clusterName'))), 5)]",
             "CUSTOM_LOCATION_NAMESPACE": "[parameters('clusterNamespace')]",
