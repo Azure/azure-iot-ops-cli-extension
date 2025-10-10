68be36b43beb145ebd834849efca5852522a27bd
Paymaun Heidari <digimaun@users.noreply.github.com>
2025-10-10 13:22:41 -0700

    increment cli build

--- azext_edge/constants.py
+++ azext_edge/constants.py
@@ -7,7 +7,7 @@
 
 import os
 
-VERSION = "2.0.0b4.dev2"
+VERSION = "2.0.0b4.dev3"
 EXTENSION_NAME = "azure-iot-ops"
 EXTENSION_ROOT = os.path.dirname(os.path.abspath(__file__))
 USER_AGENT = "IotOperationsCliExtension/{}".format(VERSION)
