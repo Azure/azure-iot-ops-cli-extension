129a7da74a7ab1058b8d29d3d530a114429a2224
Paymaun Heidari <digimaun@users.noreply.github.com>
2025-10-22 14:32:42 -0700

    build: increment aio arc extension to 1.2.112

--- azext_edge/edge/providers/orchestration/template.py
+++ azext_edge/edge/providers/orchestration/template.py
@@ -652,7 +652,7 @@ TEMPLATE_BLUEPRINT_ENABLEMENT = TemplateBlueprint(
 )
 
 TEMPLATE_BLUEPRINT_INSTANCE = TemplateBlueprint(
-    commit_id="d8c59d5b018f696bf56dc52b970a8b3499ec9d4f",
+    commit_id="2dbe066de203f20008c99402cf306dbb65c95d11",
     content={
         "$schema": "https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#",
         "languageVersion": "2.0",
@@ -1243,7 +1243,7 @@ TEMPLATE_BLUEPRINT_INSTANCE = TemplateBlueprint(
             "advancedConfig": {"$ref": "#/definitions/_1.AdvancedConfig", "defaultValue": {}},
         },
         "variables": {
-            "VERSIONS": {"iotOperations": "1.2.111"},
+            "VERSIONS": {"iotOperations": "1.2.112"},
             "TRAINS": {"iotOperations": "integration"},
             "HASH": "[coalesce(tryGet(parameters('advancedConfig'), 'resourceSuffix'), take(uniqueString(resourceGroup().id, parameters('clusterName'), parameters('clusterNamespace')), 5))]",
             "AIO_EXTENSION_SUFFIX": "[take(uniqueString(resourceId('Microsoft.Kubernetes/connectedClusters', parameters('clusterName'))), 5)]",
