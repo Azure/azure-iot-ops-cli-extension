95089257bef7aef1a26aa2e30b026bd382d0ef7e
Paymaun Heidari <digimaun@users.noreply.github.com>
2025-11-26 09:51:07 -0800

    build: increment iot ops extension to 1.2.153

--- azext_edge/edge/providers/orchestration/template.py
+++ azext_edge/edge/providers/orchestration/template.py
@@ -654,7 +654,7 @@ TEMPLATE_BLUEPRINT_ENABLEMENT = TemplateBlueprint(
 )
 
 TEMPLATE_BLUEPRINT_INSTANCE = TemplateBlueprint(
-    commit_id="29e37fb5a481c14d935f2d58498580adcf5c625e",
+    commit_id="1b033c61b45ce5fd22cf7b7336b457ab3dc1e272",
     content={
         "$schema": "https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#",
         "languageVersion": "2.0",
@@ -1243,7 +1243,7 @@ TEMPLATE_BLUEPRINT_INSTANCE = TemplateBlueprint(
             "advancedConfig": {"$ref": "#/definitions/_1.AdvancedConfig", "defaultValue": {}},
         },
         "variables": {
-            "VERSIONS": {"iotOperations": "1.2.147"},
+            "VERSIONS": {"iotOperations": "1.2.153"},
             "TRAINS": {"iotOperations": "integration"},
             "HASH": "[coalesce(tryGet(parameters('advancedConfig'), 'resourceSuffix'), take(uniqueString(resourceGroup().id, parameters('clusterName'), parameters('clusterNamespace')), 5))]",
             "AIO_EXTENSION_SUFFIX": "[take(uniqueString(resourceId('Microsoft.Kubernetes/connectedClusters', parameters('clusterName'))), 5)]",
