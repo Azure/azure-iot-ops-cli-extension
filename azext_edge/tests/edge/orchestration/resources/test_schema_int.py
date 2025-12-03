dd76e82bb7d93c07040c62c5a007694cae862184
Ryan Kelly <rykelly@microsoft.com>
2025-12-03 09:40:08 -0800

    remove schema test debugging sleep

--- azext_edge/tests/edge/orchestration/resources/test_schema_int.py
+++ azext_edge/tests/edge/orchestration/resources/test_schema_int.py
@@ -55,9 +55,6 @@ def test_schema_lifecycle(settings_with_rg, tracked_resources, tracked_files):
         f"--rn {registry_namespace} --sa-resource-id {storage_account['id']} "
     )
     tracked_resources.append(registry["id"])
-    # TODO - debugging sleep only
-    logger.info(f"Registry {registry_name} created, waiting 30s before continuing...")
-    sleep(30)
 
     # CREATE 1 with min version args
     schema_name1 = f"schema-{generate_random_string(force_lower=True, size=6)}"
