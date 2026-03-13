aebe87afbf056e114faff8d178437d3e28ca3ea0
Hangyi Wang <hangyiwang@microsoft.com>
2026-03-13 17:59:51 +0000

    test

--- azext_edge/tests/_log.py
+++ azext_edge/tests/_log.py
@@ -1,8 +1,8 @@
 # coding=utf-8
-# --------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See License.txt in the project root for license information.
-# --------------------------------------------------------------------------------------------
+# Licensed under the MIT License. See License file in the project root for license information.
+# ----------------------------------------------------------------------------------------------
 
 """
 Structured test logging helper for integration tests.
