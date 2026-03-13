3188fcc19ca72c63da20672dc87d0ddb724aa4ca
Hangyi Wang <hangyiwang@microsoft.com>
2026-03-13 14:54:46 +0000

    comment

--- azext_edge/edge/providers/adr/namespace_assets.py
+++ azext_edge/edge/providers/adr/namespace_assets.py
@@ -50,9 +50,6 @@ console = Console()
 logger = get_logger(__name__)
 NAMESPACE_ASSET_RESOURCE_TYPE = "Microsoft.DeviceRegistry/namespaces/assets"
 
-
-# Namespace-specific CSV conversion functions
-
 def _convert_sub_points_to_csv_namespace(
     sub_points: List[Dict[str, str]],
     sub_point_type: str,
