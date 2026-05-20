be7773f417eddd5f31622640ee5fa068640d1721
Hangyi Wang <hangyiwang@microsoft.com>
2026-05-20 17:42:21 +0000

    stable

--- azext_edge/tests/edge/orchestration/test_template_unit.py
+++ azext_edge/tests/edge/orchestration/test_template_unit.py
@@ -154,7 +154,7 @@ EXTENSION_CONFIGS = {
         (EXTENSION_TYPE_SSC, "1.4.1", "stable"),
     ],
     "instance": [
-        (EXTENSION_TYPE_OPS, "1.3.105", "integration"),
+        (EXTENSION_TYPE_OPS, "1.3.105", "stable"),
     ],
 }
 
